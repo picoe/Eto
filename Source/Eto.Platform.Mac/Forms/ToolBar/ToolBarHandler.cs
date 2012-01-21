a904dcb673b6c1160a5ab6f4fae76a50722e2cee
Curtis Wensley <curtis.wensley@gmail.com>
2012-01-20 22:39:57 -0800

    Mac: Allow toolbar item ID to be null (from MonoMac update)

--- Source/Eto.Platform.Mac/Forms/ToolBar/ToolBarHandler.cs
+++ Source/Eto.Platform.Mac/Forms/ToolBar/ToolBarHandler.cs
@@ -84,7 +84,7 @@ namespace Eto.Platform.Mac
 			var handler = item.Handler as IToolBarBaseItemHandler;
 			if (handler != null)
 				handler.CreateControl ();
-			Control.InsertItem (item.ID, items.Count > 0 ? items.Count - 1 : 0);
+			Control.InsertItem (item.ID ?? string.Empty, items.Count > 0 ? items.Count - 1 : 0);
 			if (handler != null)
 				handler.ControlAdded (this);
 			//Control.ValidateVisibleItems();
