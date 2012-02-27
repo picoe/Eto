4c3011f2ed667726355807192e46d4e63e896258
Carlo Kok <ck@remobjects.com>
2012-02-27 15:15:22 +0100

    PasswordBox control.

---
+++ Source/Eto/Forms/Controls/PasswordBox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Eto.Forms
+{
+	public interface IPasswordBox : ITextBox
+	{
+		char PasswordChar { get; set; }
+	}
+
+	public class PasswordBox : TextControl
+	{
+		IPasswordBox inner;
+
+		public PasswordBox() : this(Generator.Current) { }
+
+		public PasswordBox(Generator g)
+			: base(g, typeof(IPasswordBox))
+		{
+			inner = (IPasswordBox)base.Handler;
+		}
+
+		public bool ReadOnly
+		{
+			get { return inner.ReadOnly; }
+			set { inner.ReadOnly = value; }
+		}
+
+		public virtual int MaxLength
+		{
+			get { return inner.MaxLength; }
+			set { inner.MaxLength = value; }
+		}
+
+		public char PasswordChar
+		{
+			get { return inner.PasswordChar; }
+			set { inner.PasswordChar = value; }
+		}
+	}
+}
