67e90611de50f74152e2a0d2b0b4bb17979d1c8b
Curtis Wensley <curtis.wensley@gmail.com>
2012-02-27 20:56:55 -0800

    Add PasswordBoxSection to Eto.Test app

---
+++ Source/Eto.Test/Eto.Test/Sections/Controls/PasswordBoxSection.cs
@@ -0,0 +1,50 @@
+using System;
+using Eto.Forms;
+using Eto.Drawing;
+
+namespace Eto.Test.Sections.Controls
+{
+	public class PasswordBoxSection : Panel
+	{
+		public PasswordBoxSection ()
+		{
+			var layout = new DynamicLayout (this);
+
+			layout.AddRow (new Label { Text = "Default" }, Default ());
+			layout.AddRow (new Label { Text = "Read Only" }, ReadOnly ());
+			layout.AddRow (new Label { Text = "Disabled" }, Disabled ());
+
+			// growing space at end is blank!
+			layout.Add (null);
+		}
+
+		Control Default ()
+		{
+			var control = new PasswordBox { Text = "Some Text" };
+			LogEvents (control);
+			return control;
+		}
+
+		Control ReadOnly ()
+		{
+			var control = new PasswordBox { Text = "Read only text", ReadOnly = true };
+			LogEvents (control);
+			return control;
+		}
+
+		Control Disabled ()
+		{
+			var control = Default ();
+			control.Enabled = false;
+			return control;
+		}
+
+		void LogEvents (PasswordBox control)
+		{
+			control.TextChanged += delegate {
+				Log.Write (control, "TextChanged, Text: {0}", control.Text);
+			};
+		}
+	}
+}
+
