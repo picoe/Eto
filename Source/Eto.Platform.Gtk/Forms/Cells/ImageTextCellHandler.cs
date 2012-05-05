227ec96f569c50d37eeb7496a9edf86b1d2b8e0e
Curtis Wensley <curtis.wensley@gmail.com>
2012-05-04 19:13:44 -0700

    GTK: Enable editing of ImageTextCell

--- Source/Eto.Platform.Gtk/Forms/Cells/ImageTextCellHandler.cs
+++ Source/Eto.Platform.Gtk/Forms/Cells/ImageTextCellHandler.cs
@@ -15,6 +15,9 @@ namespace Eto.Platform.GtkSharp.Forms.Controls
 		{
 			imageCell = new Gtk.CellRendererPixbuf ();
 			Control = new Gtk.CellRendererText ();
+			this.Control.Edited += delegate(object o, Gtk.EditedArgs args) {
+				SetValue (args.Path, args.NewText);
+			};
 		}
 		
 		public override void AddCells (Gtk.TreeViewColumn column)
@@ -36,11 +39,14 @@ namespace Eto.Platform.GtkSharp.Forms.Controls
 		
 		public override void SetEditable (Gtk.TreeViewColumn column, bool editable)
 		{
+			this.Control.Editable = editable;
 		}
 		
 		public override void SetValue (object dataItem, object value)
 		{
-			// can't set
+			if (Widget.TextBinding != null) {
+				Widget.TextBinding.SetValue (dataItem, value as string);
+			}
 		}
 		
 		public override GLib.Value GetValue (object item, int column)
@@ -50,23 +56,24 @@ namespace Eto.Platform.GtkSharp.Forms.Controls
 					var ret = Widget.ImageBinding.GetValue (item);
 					var image = ret as Image;
 					if (image != null)
-						return new GLib.Value(((IGtkPixbuf)image.Handler).GetPixbuf (new Size (16, 16)));
+						return new GLib.Value (((IGtkPixbuf)image.Handler).GetPixbuf (new Size (16, 16)));
 				}
-				return new GLib.Value((Gdk.Pixbuf)null);
-			}
-			else if (column == textDataIndex) {
+				return new GLib.Value ((Gdk.Pixbuf)null);
+			} else if (column == textDataIndex) {
 				var ret = Widget.TextBinding.GetValue (item);
 				if (ret != null)
 					return new GLib.Value (Convert.ToString (ret));
 			}
-			return new GLib.Value((string)null);
+			return new GLib.Value ((string)null);
 		}
 		
 		public override void AttachEvent (string handler)
 		{
 			switch (handler) {
 			case GridView.EndCellEditEvent:
-				// no editing here
+				Control.Edited += (sender, e) => {
+					Source.EndCellEditing (new Gtk.TreePath (e.Path), this.ColumnIndex);
+				};
 				break;
 			default:
 				base.AttachEvent (handler);
