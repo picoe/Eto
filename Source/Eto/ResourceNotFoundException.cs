d09a5708c5700cee4e9c50165c0ce24bd0bcea92
Curtis Wensley <curtis.wensley@gmail.com>
2012-06-22 12:03:46 -0700

    Add missing ResourceNotFoundException

---
+++ Source/Eto/ResourceNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Reflection;
+
+namespace Eto
+{
+	[Serializable]
+	public class ResourceNotFoundException : EtoException
+	{
+		public ResourceNotFoundException () { }
+		public ResourceNotFoundException (string message) : base (message) { }
+		public ResourceNotFoundException (string message, Exception inner) : base (message, inner) { }
+		public ResourceNotFoundException (Assembly assembly, string resourceName)
+			: this (string.Format ("Resource '{0}' not found in assembly '{1}'", resourceName, assembly.FullName))
+		{ }
+		protected ResourceNotFoundException (SerializationInfo info, StreamingContext context) : base (info, context) { }
+	}
+}
