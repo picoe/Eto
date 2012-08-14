18d6d29ee3fab8d8e9e8fc4897f750a7bbcf1580
Curtis Wensley <curtis.wensley@gmail.com>
2012-08-13 21:51:36 -0700

    Add Sandcastle solution & monodoc build script

---
+++ Help/buildmdoc.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+BUILD_OUTPUT_PATH=$DIR/../BuildOutput
+INPUT_PATH=$BUILD_OUTPUT_PATH/Debug
+
+OUTPUT_PATH=$BUILD_OUTPUT_PATH/Docs
+MDOC_OUTPUT_PATH=$OUTPUT_PATH/mdoc
+MDOC_SOURCES_PATH=$MDOC_OUTPUT_PATH/sources
+XML_OUTPUT_PATH=$OUTPUT_PATH/xml
+
+rm -rf $MDOC_OUTPUT_PATH
+rm -rf $XML_OUTPUT_PATH
+mdoc update -i "$INPUT_PATH/Eto.XML" -o "$XML_OUTPUT_PATH" "$INPUT_PATH/Eto.dll"
+mkdir -p $MDOC_SOURCES_PATH
+mdoc assemble -o "$MDOC_SOURCES_PATH/Eto" "$XML_OUTPUT_PATH" 
+
+ETODOC='<?xml version="1.0"?>\n
+<monodoc>\n
+\t<node label="Eto.Forms" name="Eto" parent="libraries" />\n
+\t<source provider="ecma" basefile="Eto" path="Eto"/>\n
+</monodoc>'
+
+echo -e $ETODOC > "$MDOC_SOURCES_PATH/Eto.source"
+
+MONODOC='<?xml version="1.0"?>\n
+<node label="Eto.Forms Documentation" name="libraries">\n
+\t<node label="Commands and Files" name="man" />\n
+\t<node label="Languages" name="languages" />\n
+\t<node label="Tools" name="tools" />\n
+\t<node label="Various" name="various" />\n
+</node>'
+
+echo -e $MONODOC > $MDOC_OUTPUT_PATH/monodoc.xml
