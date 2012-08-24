cd937d818fa3d39fa973b17df6a076d5fb314426
Lubos Hasko <lubos@luboshasko.com>
2012-08-24 15:47:36 +1000

    Fixed ShowPrintDialog() for WPF

--- Source/Eto.Platform.Wpf/Forms/Controls/WebViewHandler.cs
+++ Source/Eto.Platform.Wpf/Forms/Controls/WebViewHandler.cs
@@ -225,14 +225,7 @@ namespace Eto.Platform.Wpf.Forms.Controls
 
         public void ShowPrintDialog ()
         {
-            var dialog = new System.Windows.Controls.PrintDialog();
-            dialog.PageRangeSelection = System.Windows.Controls.PageRangeSelection.AllPages;
-            dialog.UserPageRangeEnabled = true;
-
-            if (dialog.ShowDialog() == true)
-            {
-                dialog.PrintVisual(Control, "");
-            }
+            WebBrowser2.Document.execCommand("Print", true, null);
         }
 	}
 }
