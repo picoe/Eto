c730da308ca6a6a5669bec8e5ad80b44abde769c
Curtis Wensley <curtis.wensley@gmail.com>
2012-09-07 01:35:00 -0700

    Mac: Use bundle identifier to get ApplicationSettings special folder path, to ensure the settings go in application-specific folder

--- Source/Eto.Platform.Mac/EtoEnvironmentHandler.cs
+++ Source/Eto.Platform.Mac/EtoEnvironmentHandler.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using MonoMac.AppKit;
 using MonoMac.Foundation;
 using System.Linq;
+using Eto.Forms;
 
 namespace Eto.Platform.Mac
 {
@@ -26,12 +27,19 @@ namespace Eto.Platform.Mac
 
 		public string GetFolderPath (EtoSpecialFolder folder)
 		{
+			NSSearchPathDirectory dir;
+			NSSearchPathDomain domain;
 			switch (folder) {
 			case EtoSpecialFolder.ApplicationResources:
 				return NSBundle.MainBundle.ResourcePath;
+			case EtoSpecialFolder.ApplicationSettings:
+				Convert (folder, out dir, out domain);
+				var path = NSSearchPath.GetDirectories (dir, domain, true).FirstOrDefault ();
+				path = System.IO.Path.Combine (path, NSBundle.MainBundle.BundleIdentifier);
+				if (!System.IO.Directory.Exists (path))
+					System.IO.Directory.CreateDirectory (path);
+				return path;
 			default:
-				NSSearchPathDirectory dir;
-				NSSearchPathDomain domain;
 				Convert (folder, out dir, out domain);
 				return NSSearchPath.GetDirectories (dir, domain, true).FirstOrDefault ();
 			}
