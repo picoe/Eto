2acb1014236b03273799a37a596dcf8e26df6df0
Curtis Wensley <curtis.wensley@gmail.com>
2012-09-16 20:52:07 -0700

    Mac: Convert value for TextBoxCell to string, even it is not a string instance

--- Source/Eto.Platform.Mac/Forms/Cells/TextBoxCellHandler.cs
+++ Source/Eto.Platform.Mac/Forms/Cells/TextBoxCellHandler.cs
@@ -66,10 +66,10 @@ namespace Eto.Platform.Mac.Forms.Controls
 		{
 			if (Widget.Binding != null) {
 				var val = Widget.Binding.GetValue (dataItem);
-				return val is string ? new NSString((string)val) : null;
+				return val != null ? new NSString(Convert.ToString (val)) : null;
 			}
 			else
-				return new NSString ();
+				return null;
 		}
 		
 		public override void SetObjectValue (object dataItem, NSObject val)
