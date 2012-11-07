c4e969d9de420cd16bab0da3400846e991714cff
Curtis Wensley <curtis.wensley@gmail.com>
2012-11-06 17:47:14 -0800

    Separate fixed and scaled ImageView tests

--- Source/Eto.Test/Eto.Test/Sections/Controls/ImageViewSection.cs
+++ Source/Eto.Test/Eto.Test/Sections/Controls/ImageViewSection.cs
@@ -7,21 +7,51 @@ using System.Text;
 
 namespace Eto.Test.Sections.Controls
 {
-	public class ImageViewSection : Scrollable
+	public class ImageViewSection : Panel
 	{
 		public ImageViewSection ()
 		{
-			var layout = new DynamicLayout (this);
+			var tabs = new TabControl ();
 
+			tabs.TabPages.Add (FixedSize ());
+			tabs.TabPages.Add (ScaledSize ());
+
+			this.AddDockedControl (tabs);
+		}
+
+		TabPage FixedSize ()
+		{
+			var layout = new DynamicLayout (new Scrollable { Border = BorderType.None, ExpandContentWidth = false, ExpandContentHeight = false });
+			AddHeaders (layout);
+
+			layout.AddRow (new Label { Text = "Auto Sized" }, AutoSized (GetBitmap ()), AutoSized (GetIcon ()));
+			layout.AddRow (new Label { Text = "Small Size" }, SmallSize (GetBitmap ()), SmallSize (GetIcon ()));
+			layout.AddRow (new Label { Text = "Large Size" }, LargeSize (GetBitmap ()), LargeSize (GetIcon ()));
+
+			var page = new TabPage { Text = "Fixed Size" };
+			page.AddDockedControl (layout.Container);
+			return page;
+		}
+
+		TabPage ScaledSize ()
+		{
+			var layout = new DynamicLayout (new Scrollable { Border = BorderType.None });
+			AddHeaders (layout);
+
+			layout.AddRow (new Label { Text = "Scaled Size" }, Scaled (GetBitmap ()), Scaled (GetIcon ()));
+
+			var page = new TabPage { Text = "Scaled Size" };
+			page.AddDockedControl (layout.Container);
+			return page;
+		}
+
+		static void AddHeaders (DynamicLayout layout)
+		{
 			layout.BeginHorizontal ();
 			layout.Add (null, xscale: false);
 			layout.Add (new Label { Text = "Bitmap", HorizontalAlign = HorizontalAlign.Center }, xscale: true);
 			layout.Add (new Label { Text = "Icon", HorizontalAlign = HorizontalAlign.Center }, xscale: true);
 			layout.EndHorizontal ();
-			layout.AddRow (new Label { Text = "Auto Sized" }, AutoSized (GetBitmap ()), AutoSized (GetIcon ()));
-			layout.AddRow (new Label { Text = "Small Size" }, SmallSize (GetBitmap ()), SmallSize (GetIcon ()));
-			layout.AddRow (new Label { Text = "Large Size" }, LargeSize (GetBitmap ()), LargeSize (GetIcon ()));
-			layout.AddRow (new Label { Text = "Scaled Size" }, Scaled (GetBitmap ()), Scaled (GetIcon ()));
 		}
 
 		Icon GetIcon ()
