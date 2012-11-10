9676a8cb060ae084fbab73351347fcb8a74a40f3
Curtis Wensley <curtis.wensley@gmail.com>
2012-11-09 19:18:56 -0800

    WinForms: TreeGridView columns should autosize when expanding/collapsing nodes or data changes. Fixes #65

--- Source/Eto.Platform.Windows/Forms/Controls/WindowsControl.cs
+++ Source/Eto.Platform.Windows/Forms/Controls/WindowsControl.cs
@@ -231,12 +231,12 @@ namespace Eto.Platform.Windows
 			}
 		}
 
-		public void Invalidate ()
+		public virtual void Invalidate ()
 		{
 			Control.Invalidate (true);
 		}
 
-		public void Invalidate (Rectangle rect)
+		public virtual void Invalidate (Rectangle rect)
 		{
 			Control.Invalidate (Generator.Convert (rect), true);
 		}
