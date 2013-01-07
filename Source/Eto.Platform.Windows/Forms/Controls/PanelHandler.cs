5497c30c98fade6861fd49db71eb617284b3761f
vivek <vivekjhaveri@hotmail.com>
2013-01-07 14:55:14 +0530

    Panel handler: Pass arrow keys as input.

--- Source/Eto.Platform.Windows/Forms/Controls/PanelHandler.cs
+++ Source/Eto.Platform.Windows/Forms/Controls/PanelHandler.cs
@@ -5,11 +5,31 @@ using Eto.Forms;
 
 namespace Eto.Platform.Windows
 {
-	public class PanelHandler : WindowsContainer<SWF.Panel, Panel>, IPanel
+	public class MyPanel : SWF.Panel
+	{
+		// Need to override IsInputKey to capture 
+		// the arrow keys.
+		protected override bool IsInputKey(SWF.Keys keyData)
+		{
+			switch (keyData & SWF.Keys.KeyCode)
+			{
+				case SWF.Keys.Up:
+				case SWF.Keys.Down:
+				case SWF.Keys.Left:
+				case SWF.Keys.Right:
+				case SWF.Keys.Back:
+					return true;
+				default:
+					return base.IsInputKey(keyData);
+			}
+		}
+	}
+
+	public class PanelHandler : WindowsContainer<MyPanel, Panel>, IPanel
 	{
 		public PanelHandler()
 		{
-			Control = new SWF.Panel();
+			Control = new MyPanel();
 			this.Control.SuspendLayout ();
 			this.Control.Size = SD.Size.Empty;
 			this.Control.MinimumSize = SD.Size.Empty;
