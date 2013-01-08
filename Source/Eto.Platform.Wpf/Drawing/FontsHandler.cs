4de8786b3db252da44b78e7bc752031b88753888
Curtis Wensley <curtis.wensley@gmail.com>
2013-01-08 02:02:50 -0800

    Wpf: Implement missing FontsHandler.FontFamilyAvailable

--- Source/Eto.Platform.Wpf/Drawing/FontsHandler.cs
+++ Source/Eto.Platform.Wpf/Drawing/FontsHandler.cs
@@ -9,9 +9,22 @@ namespace Eto.Platform.Wpf.Drawing
 {
 	public class FontsHandler : WidgetHandler<Widget>, IFonts
 	{
+		HashSet<string> availableFontFamilies;
+
 		public IEnumerable<FontFamily> AvailableFontFamilies
 		{
-			get { return swm.Fonts.SystemFontFamilies.Select (r => new FontFamily(Generator, new FontFamilyHandler(r))); ; }
+			get { return swm.Fonts.SystemFontFamilies.Select (r => new FontFamily (Generator, new FontFamilyHandler (r))); ; }
+		}
+
+		public bool FontFamilyAvailable (string fontFamily)
+		{
+			if (availableFontFamilies == null) {
+				availableFontFamilies = new HashSet<string> (StringComparer.InvariantCultureIgnoreCase);
+				foreach (var family in swm.Fonts.SystemFontFamilies) {
+					availableFontFamilies.Add (family.Source);
+				}
+			}
+			return availableFontFamilies.Contains (fontFamily);
 		}
 	}
 }
