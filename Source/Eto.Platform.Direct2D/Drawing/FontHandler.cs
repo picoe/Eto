cd661cf46b042eb1b9609e3480d386bda69e3903
vivek <vivekjhaveri@hotmail.com>
2013-01-20 23:21:51 +0530

    Direct2D platform, initial checkin.

---
+++ Source/Eto.Platform.Direct2D/Drawing/FontHandler.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eto.Drawing;
+using s = SharpDX;
+using sd = SharpDX.Direct2D1;
+using sw = SharpDX.DirectWrite;
+
+namespace Eto.Platform.Direct2D.Drawing
+{
+    public class FontHandler : WidgetHandler<sw.Font, Font>, IFont
+    {
+        // Static factory
+        static sw.Factory Factory = new sw.Factory();
+        
+        private float sizeInPoints = 0f;
+        string familyName;
+        FontStyle style;
+        sw.FontStyle fontStyle;
+        sw.FontWeight fontWeight;
+
+        // These need to be disposed
+        sw.FontFace fontFace = null;
+        sw.TextFormat textFormat = null;
+
+
+        public void Create(FontTypeface typeface, float sizeInPoints)
+        {
+            this.sizeInPoints = sizeInPoints;
+            Create(typeface.Name, sizeInPoints, typeface.FontStyle);            
+        }
+
+        public void Create(SystemFont systemFont, float? sizeInPoints)
+        {
+            this.sizeInPoints = sizeInPoints ?? 0f;
+            throw new NotImplementedException();            
+        }
+
+        public void Create(FontFamily family, float sizeInPoints, FontStyle style)
+        {
+            this.sizeInPoints = sizeInPoints;
+            var familyName = family.Name;
+
+            Create(familyName, sizeInPoints, style);
+        }
+
+        private void Create(string familyName, float sizeInPoints, FontStyle style)
+        {
+            // family name
+            this.familyName = familyName;
+
+            // font style
+            this.style = style;
+
+            sw.FontStyle s;
+            sw.FontWeight w;
+            Conversions.Convert(style, out s, out w);
+
+            this.textFormat =
+                new sw.TextFormat(
+                    Factory,
+                    familyName,
+                    null, // font collection
+                    w,
+                    s,
+                    sw.FontStretch.Normal,
+                    sizeInPoints, // TODO: should this be in pixels? The documentation says device-independent pixels.
+                    "en-us");
+            
+            // Create a font collection
+            var collection = FontCollection();
+
+            int index = 0;
+            if (collection.FindFamilyName(
+                familyName: familyName,
+                index: out index))
+            {
+                // font family
+                var fontFamily =
+                    collection.GetFontFamily(index);
+
+                Conversions.Convert(
+                    style,
+                    out fontStyle,
+                    out fontWeight);
+
+                // font
+                this.Control = 
+                    fontFamily.GetFirstMatchingFont(
+                        fontWeight,
+                        sw.FontStretch.Normal,
+                        fontStyle);
+
+                // finally, the font face
+                this.fontFace = new sw.FontFace(Control);
+            }
+        }
+
+        private static sw.FontCollection fontCollection;
+        private static sw.FontCollection FontCollection()
+        {
+            if (fontCollection == null)
+                fontCollection =
+                Factory.GetSystemFontCollection(
+                    checkForUpdates: false);
+
+            return fontCollection;
+        }
+
+        public void Create()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (this.textFormat != null)
+            {
+                this.textFormat.Dispose();
+                this.textFormat = null;
+            }
+
+            if (this.fontFace != null)
+            {
+                this.fontFace.Dispose();
+                this.fontFace = null;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private sw.FontMetrics? gdiCompatibleMetrics;
+        private sw.FontMetrics GdiCompatibleMetrics
+        {
+            get
+            {
+                if(gdiCompatibleMetrics == null)
+                    gdiCompatibleMetrics =
+                        this.fontFace.GetGdiCompatibleMetrics(
+                            emSize: this.Widget.Size, // TODO: should this be in pixels?
+                            pixelsPerDip: 1,
+                            transform: null);
+
+                return gdiCompatibleMetrics.Value;
+            }
+        }
+
+        public float AscentInPixels
+        {
+            get
+            {
+                return this.GdiCompatibleMetrics.Ascent;
+            }
+        }
+
+        public float DescentInPixels
+        {
+            get { return this.GdiCompatibleMetrics.Descent; }
+        }
+
+        public float XHeightInPixels
+        {
+            get { return this.GdiCompatibleMetrics.XHeight; }
+        }
+
+        public FontFamily Family
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string FamilyName
+        {
+            get { return familyName; }
+        }
+
+        public FontStyle FontStyle
+        {
+            get { return style; }
+        }
+
+        private float? lineHeightInPixels = null;
+        public float LineHeightInPixels
+        {
+            get 
+            {
+                return AscentInPixels +
+                    DescentInPixels +
+                    GdiCompatibleMetrics.LineGap;
+            }
+        }
+
+        public float Size
+        {
+            get { return sizeInPoints; }
+        }
+
+        public FontTypeface Typeface
+        {
+            get { return null; }
+        }
+
+
+		public float XHeight
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public float Ascent
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public float Descent
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public float LineHeight
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public float Leading
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public float Baseline
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+
+		public object ControlObject
+		{
+			get { throw new NotImplementedException(); }
+		}
+	}
+}
---
+++ Source/Eto.Platform.Direct2D/Generator.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eto.Drawing;
+using Eto.Forms;
+using Eto.IO;
+using Eto.Platform.Direct2D.Drawing;
+
+namespace Eto.Platform.Direct2D
+{
+    public class Generator : Eto.Platform.Windows.Generator
+    {
+        public override string ID
+        {
+            get { return "Direct2D"; }
+        }
+
+        public Generator()
+        {
+			Eto.Platform.Windows.Generator.AddTo(this);
+
+            // This is added after the base class's assembly
+			AddTo(this);
+        }
+
+		public static new void AddTo(Eto.Generator g)
+		{
+			// Drawing
+			g.Add<IBitmap>(() => new BitmapHandler());
+			//g.Add<IFontFamily>(() => new FontFamilyHandler());
+			g.Add<IFont>(() => new FontHandler());
+			g.Add<IFonts>(() => new FontsHandler());
+			g.Add<IGraphics>(() => new GraphicsHandler());
+			g.Add<IGraphicsPathHandler>(() => new GraphicsPathHandler());
+			g.Add<IIcon>(() => new IconHandler());
+			//g.Add<IIndexedBitmap>(() => new IndexedBitmapHandler());
+			g.Add<IMatrixHandler>(() => new MatrixHandler());
+			g.Add<IPen>(() => new PenHandler());
+			//g.Add<ISolidBrush>(() => new SolidBrushHandler());
+			//g.Add<ITextureBrush>(() => new TextureBrushHandler());
+			//g.Add<ILinearGradientBrush>(() => new LinearGradientBrushHandler());
+
+			// Forms.Cells
+			//g.Add<ICheckBoxCell>(() => new CheckBoxCellHandler());
+			//g.Add<IComboBoxCell>(() => new ComboBoxCellHandler());
+			//g.Add<IImageTextCell>(() => new ImageTextCellHandler());
+			//g.Add<IImageViewCell>(() => new ImageViewCellHandler());
+			//g.Add<ITextBoxCell>(() => new TextBoxCellHandler());
+
+			// Forms.Controls
+			//g.Add<IButton>(() => new ButtonHandler());
+			//g.Add<ICheckBox>(() => new CheckBoxHandler());
+			//g.Add<IComboBox>(() => new ComboBoxHandler());
+			//g.Add<IDateTimePicker>(() => new DateTimePickerHandler());
+			//g.Add<IDrawable>(() => new DrawableHandler());
+			//g.Add<IGridColumn>(() => new GridColumnHandler());
+			//g.Add<IGridView>(() => new GridViewHandler());
+			//g.Add<IGroupBox>(() => new GroupBoxHandler());
+			//g.Add<IImageView>(() => new ImageViewHandler());
+			//g.Add<ILabel>(() => new LabelHandler());
+			//g.Add<IListBox>(() => new ListBoxHandler());
+			//g.Add<INumericUpDown>(() => new NumericUpDownHandler());
+			//g.Add<IPanel>(() => new PanelHandler());
+			//g.Add<IPasswordBox>(() => new PasswordBoxHandler());
+			//g.Add<IProgressBar>(() => new ProgressBarHandler());
+			//g.Add<IRadioButton>(() => new RadioButtonHandler());
+			//g.Add<IScrollable>(() => new ScrollableHandler());
+			//g.Add<ISlider>(() => new SliderHandler());
+			//g.Add<ISplitter>(() => new SplitterHandler());
+			//g.Add<ITabControl>(() => new TabControlHandler());
+			//g.Add<ITabPage>(() => new TabPageHandler());
+			//g.Add<ITextArea>(() => new TextAreaHandler());
+			//g.Add<ITextBox>(() => new TextBoxHandler());
+			//g.Add<ITreeGridView>(() => new TreeGridViewHandler());
+			//g.Add<ITreeView>(() => new TreeViewHandler());
+			//g.Add<IWebView>(() => new WebViewHandler());
+			//g.Add<IScreens>(() => new ScreensHandler());
+
+			// Forms.Menu
+			//g.Add<ICheckMenuItem>(() => new CheckMenuItemHandler());
+			//g.Add<IContextMenu>(() => new ContextMenuHandler());
+			//g.Add<IImageMenuItem>(() => new ImageMenuItemHandler());
+			//g.Add<IMenuBar>(() => new MenuBarHandler());
+			//g.Add<IRadioMenuItem>(() => new RadioMenuItemHandler());
+			//g.Add<ISeparatorMenuItem>(() => new SeparatorMenuItemHandler());
+
+			// Forms.Printing
+			//g.Add<IPrintDialog>(() => new PrintDialogHandler());
+			//g.Add<IPrintDocument>(() => new PrintDocumentHandler());
+			//g.Add<IPrintSettings>(() => new PrintSettingsHandler());
+
+			// Forms.ToolBar
+			//g.Add<ICheckToolBarButton>(() => new CheckToolBarButtonHandler());
+			//g.Add<ISeparatorToolBarItem>(() => new SeparatorToolBarItemHandler());
+			//g.Add<IToolBarButton>(() => new ToolBarButtonHandler());
+			//g.Add<IToolBar>(() => new ToolBarHandler());
+
+			// Forms
+			//g.Add<IApplication>(() => new ApplicationHandler());
+			//g.Add<IClipboard>(() => new ClipboardHandler());
+			//g.Add<IColorDialog>(() => new ColorDialogHandler());
+			//g.Add<ICursor>(() => new CursorHandler());
+			//g.Add<IDialog>(() => new DialogHandler());
+			//g.Add<IDockLayout>(() => new DockLayoutHandler());
+			//g.Add<IFontDialog>(() => new FontDialogHandler());
+			//g.Add<IForm>(() => new FormHandler());
+			//g.Add<IMessageBox>(() => new MessageBoxHandler());
+			//g.Add<IOpenFileDialog>(() => new OpenFileDialogHandler());
+			//g.Add<IPixelLayout>(() => new PixelLayoutHandler());
+			//g.Add<ISaveFileDialog>(() => new SaveFileDialogHandler());
+			//g.Add<ISelectFolderDialog>(() => new SelectFolderDialogHandler());
+			//g.Add<ITableLayout>(() => new TableLayoutHandler());
+			//g.Add<IUITimer>(() => new UITimerHandler());
+
+			// IO
+			//g.Add<ISystemIcons>(() => new SystemIconsHandler());
+
+			// General
+			//g.Add<IEtoEnvironment>(() => new EtoEnvironmentHandler());
+		}
+    }
+}
