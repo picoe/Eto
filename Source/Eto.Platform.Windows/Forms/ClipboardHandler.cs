88123bb979c52b0227a52b44440372cc7cb504c1
Curtis Wensley <curtis.wensley@gmail.com>
2013-01-22 18:06:56 -0800

    WinForms: Update Clipboard.Image to handle retrieving images with transparency

--- Source/Eto.Platform.Windows/Forms/ClipboardHandler.cs
+++ Source/Eto.Platform.Windows/Forms/ClipboardHandler.cs
@@ -1,27 +1,26 @@
 using System;
 using Eto.Forms;
-using SWF = System.Windows.Forms;
-using SD = System.Drawing;
+using swf = System.Windows.Forms;
+using sd = System.Drawing;
+using sdi = System.Drawing.Imaging;
 using Eto.Platform.Windows.Drawing;
 using Eto.Drawing;
+using System.Runtime.InteropServices;
 
 namespace Eto.Platform.Windows.Forms
 {
-	public class ClipboardHandler : WidgetHandler<SWF.DataObject, Clipboard>, IClipboard
+	public class ClipboardHandler : WidgetHandler<swf.DataObject, Clipboard>, IClipboard
 	{
-		
 		public ClipboardHandler ()
 		{
-			Control = new SWF.DataObject();
+			Control = new swf.DataObject();
 		}
 		
 		void Update()
 		{
-			SWF.Clipboard.SetDataObject (Control);
+			swf.Clipboard.SetDataObject (Control);
 		}
 
-		#region IClipboard implementation
-		
 		public void SetData (byte[] value, string type)
 		{
 			Control.SetData (type, value);
@@ -41,7 +40,7 @@ namespace Eto.Platform.Windows.Forms
 				Update();
 			}
 			get {
-				return SWF.Clipboard.GetText (SWF.TextDataFormat.Html);
+				return swf.Clipboard.GetText (swf.TextDataFormat.Html);
 			}
 		}
 		
@@ -52,49 +51,102 @@ namespace Eto.Platform.Windows.Forms
 				Update();
 			}
 			get {
-				return SWF.Clipboard.GetText ();				
+				return swf.Clipboard.GetText ();				
 			}
 		}
 		
 		public Image Image
 		{
-			set {
-				var sdimage = value.ControlObject as SD.Image;
-				if (sdimage != null)
-				{
-					Control.SetImage (sdimage);
-					Update();
-				}
-			}
-			get {
-				var sdimage = SWF.Clipboard.GetImage () as SD.Bitmap;
+			set
+			{
+				var sdimage = value.ControlObject as sd.Image;
 				if (sdimage != null) {
-					var handler = new BitmapHandler(sdimage);
-					return new Bitmap(Widget.Generator, handler);
+					Control.SetImage (sdimage);
+					Update ();
 				}
-				else throw new NotImplementedException();
 			}
+			get
+			{
+				Image result = null;
+				
+				try {
+					var sdimage = GetImageFromClipboard () as sd.Bitmap;
+					
+					if (sdimage != null) {
+						var handler = new BitmapHandler (sdimage);
+						
+						result = new Bitmap (Widget.Generator, handler);
+					}
+				} catch {
+				}
+				
+				return result;
+			}
+		}
+
+		/// <summary>
+		/// see http://stackoverflow.com/questions/11273669/how-to-paste-a-transparent-image-from-the-clipboard-in-a-c-sharp-winforms-app
+		/// </summary>
+		sd.Image GetImageFromClipboard ()
+		{
+			if (swf.Clipboard.GetDataObject () == null)
+				return null;
+
+			if (swf.Clipboard.GetDataObject ().GetDataPresent (swf.DataFormats.Dib)) {
+				var dib = ((System.IO.MemoryStream)swf.Clipboard.GetData (swf.DataFormats.Dib)).ToArray ();
+
+				var width = BitConverter.ToInt32 (dib, 4);
+				var height = BitConverter.ToInt32 (dib, 8);
+				var bpp = BitConverter.ToInt16 (dib, 14);
+
+				if (bpp == 32) {
+					var gch = GCHandle.Alloc (dib, GCHandleType.Pinned);
+
+					sd.Bitmap bmp = null;
+
+					try {
+						var ptr = new IntPtr ((long)gch.AddrOfPinnedObject () + 40);
+
+						bmp = new sd.Bitmap (width, height, width * 4, sdi.PixelFormat.Format32bppArgb, ptr);
+
+						var result = new sd.Bitmap (bmp);
+
+						// Images are rotated and flipped for some reason.
+						// This rotates them back.
+						result.RotateFlip (sd.RotateFlipType.Rotate180FlipX);
+
+						return result;
+					} finally {
+						gch.Free ();
+
+						if (bmp != null)
+							bmp.Dispose ();
+					}
+				}
+			}
+
+			return swf.Clipboard.ContainsImage () ? swf.Clipboard.GetImage () : null;
 		}
 
 		public byte[] GetData (string type)
 		{
-			if (SWF.Clipboard.ContainsData (type))
-				return SWF.Clipboard.GetData (type) as byte[];
+			if (swf.Clipboard.ContainsData (type))
+				return swf.Clipboard.GetData (type) as byte[];
 			else
 				return null;
 		}
 		
 		public string GetString (string type)
 		{
-			if (SWF.Clipboard.ContainsData (type))
-				return SWF.Clipboard.GetData (type) as string;
+			if (swf.Clipboard.ContainsData (type))
+				return swf.Clipboard.GetData (type) as string;
 			else
 				return null;
 		}
 
 		public string[] Types {
 			get {
-				var data = SWF.Clipboard.GetDataObject ();
+				var data = swf.Clipboard.GetDataObject ();
 				if (data != null)
 					return data.GetFormats ();
 				else
@@ -104,12 +156,9 @@ namespace Eto.Platform.Windows.Forms
 		
 		public void Clear ()
 		{
-			SWF.Clipboard.Clear ();
-			Control = new SWF.DataObject();
+			swf.Clipboard.Clear ();
+			Control = new swf.DataObject();
 		}
-		
-		#endregion
-
 	}
 }
 
