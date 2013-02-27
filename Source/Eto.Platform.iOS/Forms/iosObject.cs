2a7b66092039baae5b51d879d37cd64a0adbb07c
Curtis Wensley <curtis.wensley@gmail.com>
2013-02-26 16:01:22 -0800

    Mac/iOS: Share observer logic for both platforms

--- Source/Eto.Platform.iOS/Forms/iosObject.cs
+++ Source/Eto.Platform.iOS/Forms/iosObject.cs
@@ -6,80 +6,28 @@ using MonoTouch.ObjCRuntime;
 
 namespace Eto.Platform.iOS.Forms
 {
-	[Register("ControlObserver")]
-	class ControlObserver : NSObject
-	{
-		public Action Action { get; set; }
-		public NSString KeyPath { get; set; }
-		public NSObject Control { get; set; }
-		
-		public override void ObserveValue (NSString keyPath, NSObject ofObject, NSDictionary change, IntPtr context)
-		{
-			Action();
-		}
-	}
-
-	public class iosObject<T, W> : WidgetHandler<T, W>
+	public class iosObject<T, W> : MacBase<T, W>
 		where T: NSObject 
 		where W: InstanceWidget
 	{
-		List<NSObject> notifications;
-		List<ControlObserver> observers;
-
-		public void AddMethod (IntPtr selector, Delegate action, string arguments, object control = null)
+		public virtual object EventObject
 		{
-			control = control ?? Control;
-			var type = control.GetType ();
-			/*if (!typeof(IMacControl).IsAssignableFrom (type))
-				throw new EtoException("Control does not inherit from IMacControl");*/
-			var cls = new Class(type);
-			cls.AddMethod (selector, action, arguments);
+			get { return Control; }
 		}
 
-		protected void AddObserver(NSString key, Action<NSNotification> notification)
+		public new void AddMethod (Selector selector, Delegate action, string arguments, object control = null)
 		{
-			if (notifications == null) notifications = new List<NSObject>();
-			notifications.Add(NSNotificationCenter.DefaultCenter.AddObserver(key, notification, Control));
-		}
-
-		protected void AddObserver(NSObject obj, NSString key, Action<NSNotification> notification)
-		{
-			if (notifications == null) notifications = new List<NSObject>();
-			notifications.Add(NSNotificationCenter.DefaultCenter.AddObserver(key, notification, obj));
+			base.AddMethod (selector, action, arguments, control ?? EventObject);
 		}
 		
-		protected void AddControlObserver(NSString key, Action action)
+		public new NSObject AddObserver (NSString key, Action<ObserverActionArgs> action, NSObject control = null)
 		{
-			if (observers == null) observers = new List<ControlObserver>();
-			var observer = new ControlObserver{ Action = action, KeyPath = key, Control = this.Control };
-			observers.Add (observer);
-			//Console.WriteLine ("{0}: 3. Adding observer! {1}, {2}", this.WidgetID, this.GetType (), Control.GetHashCode ());
-			Control.AddObserver(observer, key, NSKeyValueObservingOptions.New, IntPtr.Zero);
+			return base.AddObserver (key, action, control ?? Control);
 		}
-
-		void DisposeInternal ()
+		
+		public new void AddControlObserver (NSString key, Action<ObserverActionArgs> action, NSObject control = null)
 		{
-			if (observers != null) {
-				foreach (var observer in observers) {
-					//Console.WriteLine ("{0}: 4. Removing observer! {1}, {2}", this.WidgetID, this.GetType (), observer.Control.GetHashCode ());
-					Control.RemoveObserver(observer, observer.KeyPath);
-				}
-				observers = null;
-			}
-
-			// dispose in finalizer as well
-			if (notifications != null) {
-				NSNotificationCenter.DefaultCenter.RemoveObservers(notifications);
-				notifications = null;
-			}
-		}
-
-		protected override void Dispose (bool disposing)
-		{
-			if (disposing)
-				DisposeInternal ();
-
-			base.Dispose (disposing);
+			base.AddControlObserver (key, action, control ?? Control);
 		}
 	}
 }
