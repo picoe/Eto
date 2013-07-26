a016db1f4aff577964ae2677445e89fb3620964c
vivek <vivekjhaveri@hotmail.com>
2013-07-26 10:21:54 +0530

    DataStoreView, in progress.

--- Source/Eto/Forms/IDataStore.cs
+++ Source/Eto/Forms/IDataStore.cs
@@ -89,4 +89,94 @@ namespace Eto.Forms
 			}
 		}
 	}
+
+	/// <summary>
+	/// Provides sorting and filtering to a data store.
+	/// 
+	/// TODO: should this be made generic, to handle
+	/// an IDataStore of T?
+	/// </summary>
+	public interface IDataStoreView
+	{
+		/// <summary>
+		/// The model store. This is the input to the IDataStoreView.
+		/// </summary>
+		IDataStore Model { get; set; }
+
+		/// <summary>
+		/// The filtered and sorted view of the model datastore.
+		/// The IDataStoreView creates this when Model is set.
+		/// View's object reference does not change until the
+		/// model is reset. The contents of View change
+		/// when Model's contents change or when SortComparer or
+		/// Filter change.
+		/// </summary>
+		IDataStore View { get; }
+
+		/// <summary>
+		/// Converts the index of an object in the view to an
+		/// index of the same object in the model. This method
+		/// always succeeds since the view is a subset of the model.
+		/// </summary>
+		int ViewToModel(int index);
+
+		/// <summary>
+		/// Converts the index of an object in the model to an
+		/// index of the same object in the view. 
+		/// 
+		/// Returns -1 if the object is not in the view because
+		/// it is filtered out.
+		/// </summary>
+		int ModelToView(int index);
+
+		/// <summary>
+		/// If non-null, sorts the model using this comparer.
+		/// </summary>
+		Comparison<object> SortComparer { get; set; }
+
+		/// <summary>
+		/// If non-null, excludes objects from the view for which
+		/// Filter returns false.
+		/// </summary>
+		Func<object, bool> Filter { get; set; }
+	}
+
+	public class DataStoreView : IDataStoreView
+	{
+		public IDataStore model;
+		public IDataStore Model
+		{
+			get { return model; }
+			set { model = value; }
+		}
+
+		public IDataStore View
+		{
+			get { return model; }
+		}
+
+		public int ViewToModel(int index)
+		{
+			return index;
+		}
+
+		public int ModelToView(int index)
+		{
+			return index;
+		}
+
+		private Comparison<object> sortComparer;
+		public Comparison<object> SortComparer
+		{
+			get { return sortComparer; }
+			set { sortComparer = value; }
+		}
+
+		private Func<object, bool> filter;
+		public Func<object, bool> Filter
+		{
+			get { return filter; }
+			set { filter = value; }
+		}
+	}
 }
