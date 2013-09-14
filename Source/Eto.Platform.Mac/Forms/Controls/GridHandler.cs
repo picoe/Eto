fa4db75cd6838bc778836797d06905d685cb55f0
Curtis Wensley <curtis.wensley@gmail.com>
2013-09-14 15:15:36 -0700

    Mac: GridColumn with AutoSize set to true, size to content when scrolling

--- Source/Eto.Platform.Mac/Forms/Controls/GridHandler.cs
+++ Source/Eto.Platform.Mac/Forms/Controls/GridHandler.cs
@@ -13,68 +13,75 @@ namespace Eto.Platform.Mac.Forms.Controls
 	public interface IGridHandler
 	{
 		Grid Widget { get; }
-		
+
 		NSTableView Table { get; }
 	}
-	
+
 	class EtoScrollView : NSScrollView
 	{
 		public IGridHandler Handler { get; set; }
 
 		bool autoSized;
-		
-		public override void SetFrameSize (System.Drawing.SizeF newSize)
+
+		public override void SetFrameSize(System.Drawing.SizeF newSize)
 		{
-			base.SetFrameSize (newSize);
-			if (!autoSized && Handler.Widget.Loaded) {
-				var rect = Handler.Table.VisibleRect ();
-				if (!rect.IsEmpty) {
-					foreach (var col in Handler.Widget.Columns) {
-						((GridColumnHandler)col.Handler).Resize ();
+			base.SetFrameSize(newSize);
+			if (!autoSized && Handler.Widget.Loaded)
+			{
+				var rect = Handler.Table.VisibleRect();
+				if (!rect.IsEmpty)
+				{
+					foreach (var col in Handler.Widget.Columns)
+					{
+						((GridColumnHandler)col.Handler).Resize();
 					}
 					autoSized = true;
 				}
 			}
 		}
 	}
-	
+
 	class EtoTableHeaderView : NSTableHeaderView
 	{
 		public IGridHandler Handler { get; set; }
-			
-		public override void MouseDown (NSEvent theEvent)
+
+		public override void MouseDown(NSEvent theEvent)
 		{
-			var col = base.GetColumn (base.ConvertPointFromBase (theEvent.LocationInWindow));
-			if (col >= 0) {
-				var column = Handler.Widget.Columns [col];
+			var col = base.GetColumn(base.ConvertPointFromBase(theEvent.LocationInWindow));
+			if (col >= 0)
+			{
+				var column = Handler.Widget.Columns[col];
 				if (!column.Sortable)
 					return;
 			}
-			base.MouseDown (theEvent);
+			base.MouseDown(theEvent);
 		}
 	}
 
 	class MacCellFormatArgs : GridCellFormatEventArgs
 	{
 		Font font;
-		
+
 		public ICellHandler CellHandler { get { return Column.DataCell.Handler as ICellHandler; } }
-		
+
 		public NSCell Cell { get; private set; }
-		
+
 		public MacCellFormatArgs(GridColumn column, object item, int row, NSCell cell)
 			: base(column, item, row)
 		{
 			this.Cell = cell;
 		}
-		
-		public override Font Font {
-			get {
+
+		public override Font Font
+		{
+			get
+			{
 				if (font == null)
-					font = new Font (CellHandler.Generator, new FontHandler (Cell.Font));
+					font = new Font(CellHandler.Generator, new FontHandler(Cell.Font));
 				return font;
 			}
-			set {
+			set
+			{
 				font = value;
 				if (font != null)
 					Cell.Font = ((FontHandler)font.Handler).Control;
@@ -82,19 +89,20 @@ namespace Eto.Platform.Mac.Forms.Controls
 					Cell.Font = null;
 			}
 		}
-		
-		public override Color BackgroundColor {
-			get { return CellHandler.GetBackgroundColor (Cell); }
-			set { CellHandler.SetBackgroundColor (Cell, value); }
+
+		public override Color BackgroundColor
+		{
+			get { return CellHandler.GetBackgroundColor(Cell); }
+			set { CellHandler.SetBackgroundColor(Cell, value); }
 		}
-		
-		public override Color ForegroundColor {
-			get { return CellHandler.GetForegroundColor (Cell); }
-			set { CellHandler.SetForegroundColor (Cell, value); }
+
+		public override Color ForegroundColor
+		{
+			get { return CellHandler.GetForegroundColor(Cell); }
+			set { CellHandler.SetForegroundColor(Cell, value); }
 		}
 	}
 
-	
 	public abstract class GridHandler<T, W> : MacControl<T, W>, IGrid, IDataViewHandler, IGridHandler
 		where T: NSTableView
 		where W: Grid
@@ -102,173 +110,208 @@ namespace Eto.Platform.Mac.Forms.Controls
 		ColumnCollection columns;
 		ContextMenu contextMenu;
 
-		public NSTableView Table {
+		public NSTableView Table
+		{
 			get { return Control; }
 		}
-		
+
 		public NSScrollView ScrollView { get; private set; }
-		
+
 		public override NSView ContainerControl { get { return ScrollView; } }
-		
-		protected virtual void PreUpdateColumn (int index)
-		{
-		}
-		
-		protected virtual void UpdateColumns ()
+
+		protected virtual void PreUpdateColumn(int index)
 		{
 		}
 
-		public GridColumnHandler GetColumn (NSTableColumn tableColumn)
+		protected virtual void UpdateColumns()
+		{
+		}
+
+		public GridColumnHandler GetColumn(NSTableColumn tableColumn)
 		{
 			var str = tableColumn.Identifier;
-			if (!string.IsNullOrEmpty (str)) {
+			if (!string.IsNullOrEmpty(str))
+			{
 				int col;
-				if (int.TryParse (str, out col)) {
-					return GetColumn (col);
+				if (int.TryParse(str, out col))
+				{
+					return GetColumn(col);
 				}
 			}
 			return null;
 		}
 
-		public GridColumnHandler GetColumn (int column)
+		public GridColumnHandler GetColumn(int column)
 		{
 			return Widget.Columns[column].Handler as GridColumnHandler;
 			//return Widget.Columns.Select (r => r.Handler as GridColumnHandler).First (r => r.Column == column);
 		}
-		
+
 		class ColumnCollection : EnumerableChangedHandler<GridColumn, GridColumnCollection>
 		{
 			public GridHandler<T,W> Handler { get; set; }
-			
-			public override void AddItem (GridColumn item)
+
+			public override void AddItem(GridColumn item)
 			{
 				var colhandler = (GridColumnHandler)item.Handler;
-				Handler.Control.AddColumn (colhandler.Control);
-				colhandler.Setup (Handler.Control.ColumnCount - 1);
+				Handler.Control.AddColumn(colhandler.Control);
+				colhandler.Setup(Handler.Control.ColumnCount - 1);
 				
-				Handler.UpdateColumns ();
+				Handler.UpdateColumns();
 			}
 
-			public override void InsertItem (int index, GridColumn item)
+			public override void InsertItem(int index, GridColumn item)
 			{
 				var outline = Handler.Control;
-				var columns = new List<NSTableColumn> (outline.TableColumns ());
-				Handler.PreUpdateColumn (index);
-				for (int i = index; i < columns.Count; i++) {
-					outline.RemoveColumn (columns [i]);
+				var columns = new List<NSTableColumn>(outline.TableColumns());
+				Handler.PreUpdateColumn(index);
+				for (int i = index; i < columns.Count; i++)
+				{
+					outline.RemoveColumn(columns[i]);
 				}
 				var colhandler = (GridColumnHandler)item.Handler;
-				columns.Insert (index, colhandler.Control);
-				outline.AddColumn (colhandler.Control);
-				colhandler.Setup (index);
-				for (int i = index + 1; i < columns.Count; i++) {
-					var col = columns [i];
-					var colHandler = Handler.GetColumn (i);
-					colHandler.Setup (i);
-					outline.AddColumn (col);
+				columns.Insert(index, colhandler.Control);
+				outline.AddColumn(colhandler.Control);
+				colhandler.Setup(index);
+				for (int i = index + 1; i < columns.Count; i++)
+				{
+					var col = columns[i];
+					var colHandler = Handler.GetColumn(i);
+					colHandler.Setup(i);
+					outline.AddColumn(col);
 				}
-				Handler.UpdateColumns ();
+				Handler.UpdateColumns();
 			}
 
-			public override void RemoveItem (int index)
+			public override void RemoveItem(int index)
 			{
 				var outline = Handler.Control;
-				var columns = new List<NSTableColumn> (outline.TableColumns ());
-				Handler.PreUpdateColumn (index);
-				for (int i = index; i < columns.Count; i++) {
-					outline.RemoveColumn (columns [i]);
+				var columns = new List<NSTableColumn>(outline.TableColumns());
+				Handler.PreUpdateColumn(index);
+				for (int i = index; i < columns.Count; i++)
+				{
+					outline.RemoveColumn(columns[i]);
 				}
-				columns.RemoveAt (index);
-				for (int i = index; i < columns.Count; i++) {
-					var col = columns [i];
-					var colHandler = Handler.GetColumn (i);
-					colHandler.Setup (i);
-					outline.AddColumn (col);
+				columns.RemoveAt(index);
+				for (int i = index; i < columns.Count; i++)
+				{
+					var col = columns[i];
+					var colHandler = Handler.GetColumn(i);
+					colHandler.Setup(i);
+					outline.AddColumn(col);
 				}
-				Handler.UpdateColumns ();
+				Handler.UpdateColumns();
 			}
 
-			public override void RemoveAllItems ()
+			public override void RemoveAllItems()
 			{
-				Handler.PreUpdateColumn (0);
+				Handler.PreUpdateColumn(0);
 				foreach (var col in Handler.Control.TableColumns ())
-					Handler.Control.RemoveColumn (col);
-				Handler.UpdateColumns ();
+					Handler.Control.RemoveColumn(col);
+				Handler.UpdateColumns();
 			}
-
 		}
 
-		
-		public GridHandler ()
+		public GridHandler()
 		{
-			ScrollView = new EtoScrollView {
+			ScrollView = new EtoScrollView
+			{
 				Handler = this,
 				HasVerticalScroller = true,
 				HasHorizontalScroller = true,
 				AutohidesScrollers = true,
 				BorderType = NSBorderType.BezelBorder
 			};
+
+			ScrollView.ContentView.PostsBoundsChangedNotifications = true;
+			this.AddObserver(NSView.NSViewBoundsDidChangeNotification, e => {
+				var handler = (GridHandler<T,W>)e.Handler;
+				if (handler.Widget.Loaded)
+				{
+					var rect = handler.Table.VisibleRect();
+					if (!rect.IsEmpty)
+					{
+						foreach (var col in handler.Widget.Columns)
+						{
+							((GridColumnHandler)col.Handler).Resize();
+						}
+					}
+				}
+			}, ScrollView.ContentView);
+
 		}
-		
-		public override void AttachEvent (string handler)
+
+		public override void AttachEvent(string handler)
 		{
-			switch (handler) {
-			default:
-				base.AttachEvent (handler);
-				break;
+			switch (handler)
+			{
+				default:
+					base.AttachEvent(handler);
+					break;
 			}
 		}
 
-		protected override void Initialize ()
+		protected override void Initialize()
 		{
-			base.Initialize ();
+			base.Initialize();
 			columns = new ColumnCollection { Handler = this };
-			columns.Register (Widget.Columns);
+			columns.Register(Widget.Columns);
 
 			Control.HeaderView = new EtoTableHeaderView { Handler = this };
 			ScrollView.DocumentView = Control;
 		}
-		
-		public override void OnLoadComplete (EventArgs e)
+
+		public override void OnLoadComplete(EventArgs e)
 		{
-			base.OnLoadComplete (e);
+			base.OnLoadComplete(e);
 			
 			int i = 0;
-			foreach (var col in this.Widget.Columns) {
+			foreach (var col in this.Widget.Columns)
+			{
 				var colHandler = (GridColumnHandler)col.Handler;
-				colHandler.Loaded (this, i++);
-				colHandler.Resize ();
+				colHandler.Loaded(this, i++);
+				colHandler.Resize();
 			}
 		}
 
-		public void ResizeAllColumns ()
+		public void ResizeAllColumns()
 		{
-			foreach (var col in Widget.Columns.Select (r => r.Handler as GridColumnHandler)) {
+			foreach (var col in Widget.Columns.Select (r => r.Handler as GridColumnHandler))
+			{
 				col.Resize();
 			}
 		}
 
-		public bool ShowHeader {
-			get {
+		public bool ShowHeader
+		{
+			get
+			{
 				return Control.HeaderView != null;
 			}
-			set {
-				if (value && Control.HeaderView == null) {
+			set
+			{
+				if (value && Control.HeaderView == null)
+				{
 					Control.HeaderView = new EtoTableHeaderView { Handler = this };
-				} else if (!value && Control.HeaderView != null) {
+				}
+				else if (!value && Control.HeaderView != null)
+				{
 					Control.HeaderView = null;
 				}
 			}
 		}
 
-		public bool AllowColumnReordering {
+		public bool AllowColumnReordering
+		{
 			get { return Control.AllowsColumnReordering; }
 			set { Control.AllowsColumnReordering = value; }
 		}
-		
-		public ContextMenu ContextMenu {
+
+		public ContextMenu ContextMenu
+		{
 			get { return contextMenu; }
-			set {
+			set
+			{
 				contextMenu = value;
 				if (contextMenu != null)
 					Control.Menu = ((ContextMenuHandler)contextMenu.Handler).Control;
@@ -277,38 +320,41 @@ namespace Eto.Platform.Mac.Forms.Controls
 			}
 		}
 
-		public bool AllowMultipleSelection {
+		public bool AllowMultipleSelection
+		{
 			get { return Control.AllowsMultipleSelection; }
 			set { Control.AllowsMultipleSelection = value; }
 		}
 
-		public IEnumerable<int> SelectedRows {
-			get { 
+		public IEnumerable<int> SelectedRows
+		{
+			get
+			{ 
 				if (Control.SelectedRows != null && Control.SelectedRows.Count > 0)
-					return Control.SelectedRows.Select (r => (int)r);
+					return Control.SelectedRows.Select(r => (int)r);
 				else
 					return Enumerable.Empty<int>();
 			}
 		}
 
-		public void SelectAll ()
+		public void SelectAll()
 		{
-			Control.SelectAll (Control);
+			Control.SelectAll(Control);
 		}
 
-		public void SelectRow (int row)
+		public void SelectRow(int row)
 		{
-			Control.SelectRow (row, false);
+			Control.SelectRow(row, false);
 		}
 
-		public void UnselectRow (int row)
+		public void UnselectRow(int row)
 		{
-			Control.DeselectRow (row);
+			Control.DeselectRow(row);
 		}
 
-		public void UnselectAll ()
+		public void UnselectAll()
 		{
-			Control.DeselectAll (Control);
+			Control.DeselectAll(Control);
 		}
 
 		public int RowHeight
@@ -316,26 +362,32 @@ namespace Eto.Platform.Mac.Forms.Controls
 			get { return (int)Control.RowHeight; }
 			set { Control.RowHeight = value; }
 		}
-		
-		public abstract object GetItem (int row);
-		
-		public virtual int RowCount {
+
+		public abstract object GetItem(int row);
+
+		public virtual int RowCount
+		{
 			get { return Control.RowCount; }
 		}
-		
-		Grid IGridHandler.Widget {
+
+		Grid IGridHandler.Widget
+		{
 			get { return Widget; }
 		}
-		
-		public System.Drawing.RectangleF GetVisibleRect ()
+
+		public System.Drawing.RectangleF GetVisibleRect()
 		{
-			return ScrollView.VisibleRect ();
+			var rect = ScrollView.VisibleRect();
+			var loc = ScrollView.ContentView.Bounds.Location;
+			rect.Offset(loc);
+			return rect;
 		}
 
-		protected override Size GetNaturalSize (Size availableSize)
+		protected override Size GetNaturalSize(Size availableSize)
 		{
-			var width = Widget.Columns.Sum (r => r.Width);
-			if (width == 0) width = 100;
+			var width = Widget.Columns.Sum(r => r.Width);
+			if (width == 0)
+				width = 100;
 			var height = this.RowHeight * 10;
 			return new Size(width, height);
 		}
