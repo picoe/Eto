11153ae77e32962de19e0ea82bc1c43027e26558
Curtis Wensley <curtis.wensley@gmail.com>
2013-09-26 16:04:20 -0700

    Eto.Test Sections should not be responsible for where the content goes, as this ties the MainForm implementation to the section list too closely. WindowSectionMethod now shows a button to open the window/dialog, as opening them automatically is too jarring

--- Source/Eto.Test/Eto.Test/MainForm.cs
+++ Source/Eto.Test/Eto.Test/MainForm.cs
@@ -74,7 +74,29 @@ namespace Eto.Test
 				SectionList.Focus();
 			};
 			SectionList.SelectedItemChanged += (sender, e) => {
-				SectionList.Show(navigation, contentContainer);
+				try
+				{
+					var item = SectionList.SelectedItem;
+					Control content = item != null ? item.CreateContent() : null;
+
+					if (navigation != null)
+					{
+						if (content != null)
+							navigation.Push(content, item != null ? item.Text : null);
+					}
+					else
+					{
+						contentContainer.SuspendLayout();
+						contentContainer.Content = content;
+						contentContainer.ResumeLayout();
+					}
+				}
+				catch (Exception ex)
+				{
+					Log.Write(this, "Error loading section: {0}", ex.InnerException != null ? ex.InnerException : ex);
+					contentContainer.Content = null;
+				}
+
 				#if DEBUG
 				GC.Collect();
 				GC.WaitForPendingFinalizers();
