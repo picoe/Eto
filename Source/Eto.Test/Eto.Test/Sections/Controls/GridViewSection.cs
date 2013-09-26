e2aafeaea8a34687d3f48d7b9b3088a4498d29aa
Curtis Wensley <curtis.wensley@gmail.com>
2013-09-26 16:31:38 -0700

    Minor cleanup

--- Source/Eto.Test/Eto.Test/Sections/Controls/GridViewSection.cs
+++ Source/Eto.Test/Eto.Test/Sections/Controls/GridViewSection.cs
@@ -40,18 +40,21 @@ namespace Eto.Test.Sections.Controls
 			layout.Add(filterText = new SearchBox { PlaceholderText = "Filter" });
 			var withContextMenuAndFilter = WithContextMenuAndFilter();
 			layout.Add(withContextMenuAndFilter);
-			var selectionGridView = Default(addItems: false);
 			layout.EndVertical();
 			layout.EndHorizontal();
+
+			var selectionGridView = Default(addItems: false);
 			layout.BeginHorizontal();
 			layout.Add(new Label { Text = "Selected Items" });
 			layout.Add(selectionGridView);
+			layout.EndHorizontal();
+
+			// hook up selection of main grid to the selection grid
 			withContextMenuAndFilter.SelectionChanged += (s, e) => {
 				var items = new GridItemCollection();
 				items.AddRange(withContextMenuAndFilter.SelectedItems);
 				selectionGridView.DataStore = items;
 			};
-			layout.EndHorizontal();
 #endif
 
 			Content = layout;
@@ -211,12 +214,11 @@ namespace Eto.Test.Sections.Controls
 
 					// Every item in the split filter string should be within the Text property
 					foreach (var filterItem in filterItems)
-						if (!i.Text.Contains(filterItem))
+						if (i.Text.IndexOf(filterItem, StringComparison.CurrentCultureIgnoreCase) == -1)
 						{
 							matches = false;
 							break;
 						}
-					;
 
 					return matches;
 				};
