7a4904a0d1fcfa457809b5d735e744eeb820ca7d
Curtis Wensley <curtis.wensley@gmail.com>
2013-10-10 22:40:15 -0700

    Wpf: Use positive infinity when measuring combo box to attempt to fix #83

--- Source/Eto.Platform.Wpf/Forms/Controls/ComboBoxHandler.cs
+++ Source/Eto.Platform.Wpf/Forms/Controls/ComboBoxHandler.cs
@@ -12,95 +12,98 @@ using System.Collections;
 
 namespace Eto.Platform.Wpf.Forms.Controls
 {
-	public class ComboBoxHandler : WpfControl<swc.ComboBox, ComboBox>, IComboBox
-	{
-		IListStore store;
+    public class ComboBoxHandler : WpfControl<swc.ComboBox, ComboBox>, IComboBox
+    {
+        IListStore store;
 
-		public class EtoComboBox : swc.ComboBox
-		{
-			int? _selected;
+        public class EtoComboBox : swc.ComboBox
+        {
+            int? _selected;
 
-			public EtoComboBox ()
-			{
-				Loaded += ComboBoxEx_Loaded;
-			}
+            public EtoComboBox()
+            {
+                Loaded += ComboBoxEx_Loaded;
+            }
 
-			public override void OnApplyTemplate ()
-			{
-				base.OnApplyTemplate ();
+            public override void OnApplyTemplate()
+            {
+                base.OnApplyTemplate();
 
-				_selected = SelectedIndex;
-				SelectedIndex = -1;
-			}
+                _selected = SelectedIndex;
+                SelectedIndex = -1;
+            }
 
-			protected override void OnSelectionChanged (swc.SelectionChangedEventArgs e)
-			{
-				if (_selected == null)
-					base.OnSelectionChanged (e);
-			}
+            protected override void OnSelectionChanged(swc.SelectionChangedEventArgs e)
+            {
+                if (_selected == null)
+                    base.OnSelectionChanged(e);
+            }
 
-			protected override void OnItemsChanged (System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-			{
-				base.OnItemsChanged (e);
-				if (this.IsLoaded) {
-					this.InvalidateMeasure ();
-				}
-			}
+            protected override void OnItemsChanged(System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+            {
+                base.OnItemsChanged(e);
+                if (this.IsLoaded)
+                {
+                    this.InvalidateMeasure();
+                }
+            }
 
-			void ComboBoxEx_Loaded (object sender, sw.RoutedEventArgs e)
-			{
-				if (_selected != null) {
-					SelectedIndex = _selected.Value;
-					_selected = null;
-				}
-			}
+            void ComboBoxEx_Loaded(object sender, sw.RoutedEventArgs e)
+            {
+                if (_selected != null)
+                {
+                    SelectedIndex = _selected.Value;
+                    _selected = null;
+                }
+            }
 
-			protected override sw.Size MeasureOverride (sw.Size constraint)
-			{
-				var size = base.MeasureOverride (constraint);
+            protected override sw.Size MeasureOverride(sw.Size constraint)
+            {
+                var size = base.MeasureOverride(constraint);
 
-				var popup = GetTemplateChild ("PART_Popup") as swc.Primitives.Popup;
-				var content = popup.Child as sw.FrameworkElement;
-				content.Measure (constraint);
-				size.Width = Math.Min(constraint.Width, Math.Max (content.DesiredSize.Width, size.Width));
-				return size;
-			}
+                var popup = GetTemplateChild("PART_Popup") as swc.Primitives.Popup;
+                var content = popup.Child as sw.FrameworkElement;
+                content.Measure(Conversions.PositiveInfinitySize);
+                size.Width = Math.Min(constraint.Width, Math.Max(content.DesiredSize.Width, size.Width));
+                return size;
+            }
 
-		}
+        }
 
-		public ComboBoxHandler ()
-		{
-			Control = new EtoComboBox ();
-			var template = new sw.DataTemplate (typeof (IListItem));
-			template.VisualTree = WpfListItemHelper.TextBlock ();
-			Control.ItemTemplate = template;
-		}
+        public ComboBoxHandler()
+        {
+            Control = new EtoComboBox();
+            var template = new sw.DataTemplate(typeof(IListItem));
+            template.VisualTree = WpfListItemHelper.TextBlock();
+            Control.ItemTemplate = template;
+        }
 
-		public override bool UseMousePreview { get { return true; } }
+        public override bool UseMousePreview { get { return true; } }
 
-		public override void OnLoad (EventArgs e)
-		{
-			base.OnLoad (e);
-			Control.SelectionChanged += delegate {
-				Widget.OnSelectedIndexChanged (EventArgs.Empty);
-			};
-		}
+        public override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Control.SelectionChanged += delegate
+            {
+                Widget.OnSelectedIndexChanged(EventArgs.Empty);
+            };
+        }
 
 
-		public IListStore DataStore
-		{
-			get { return store; }
-			set
-			{
-				store = value;
-				Control.ItemsSource = store as IEnumerable ?? store.AsEnumerable ();
-			}
-		}
+        public IListStore DataStore
+        {
+            get { return store; }
+            set
+            {
+                store = value;
+                Control.ItemsSource = store as IEnumerable ?? store.AsEnumerable();
+            }
+        }
 
-		public int SelectedIndex
-		{
-			get { return Control.SelectedIndex; }
-			set { Control.SelectedIndex = value; }
-		}
-	}
+        public int SelectedIndex
+        {
+            get { return Control.SelectedIndex; }
+            set { Control.SelectedIndex = value; }
+        }
+    }
 }
