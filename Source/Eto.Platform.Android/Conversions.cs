70cbfada92da1d7ac028f67f663eef4367d0eb83
vivek <vivekjhaveri@hotmail.com>
2013-10-13 02:31:23 +0530

    Android PenHandler, in progress.

--- Source/Eto.Platform.Android/Conversions.cs
+++ Source/Eto.Platform.Android/Conversions.cs
@@ -29,6 +29,63 @@ namespace Eto.Platform.Android
 		{
 			return new ag.Color((int)color.ToArgb());
 		}
+
+		public static ag.Paint ToAndroid(this Pen pen)
+		{
+			return (ag.Paint)pen.ControlObject;
+		}
+
+		public static ag.Paint.Join ToAndroid(this PenLineJoin value)
+		{
+			switch (value)
+			{
+				case PenLineJoin.Miter:
+					return ag.Paint.Join.Miter;
+				case PenLineJoin.Bevel:
+					return ag.Paint.Join.Bevel;
+				case PenLineJoin.Round:
+					return ag.Paint.Join.Round;
+				default:
+					throw new NotSupportedException();
+			}
+		}
+
+		public static PenLineJoin ToEto(this ag.Paint.Join value)
+		{
+			if(object.ReferenceEquals(value, ag.Paint.Join.Bevel))
+				return PenLineJoin.Bevel;
+			if(object.ReferenceEquals(value, ag.Paint.Join.Miter))
+				return PenLineJoin.Miter;
+			if(object.ReferenceEquals(value, ag.Paint.Join.Round))
+				return PenLineJoin.Round;
+			throw new NotSupportedException();
+		}
+
+		public static ag.Paint.Cap ToSD(this PenLineCap value)
+		{
+			switch (value)
+			{
+				case PenLineCap.Butt:
+					return ag.Paint.Cap.Butt;
+				case PenLineCap.Round:
+					return ag.Paint.Cap.Round;
+				case PenLineCap.Square:
+					return ag.Paint.Cap.Square;
+				default:
+					throw new NotSupportedException();
+			}
+		}
+
+		public static PenLineCap ToEto(this ag.Paint.Cap value)
+		{
+				if(object.ReferenceEquals(value, ag.Paint.Cap.Butt))
+					return PenLineCap.Butt;
+				if(object.ReferenceEquals(value, ag.Paint.Cap.Round))
+					return PenLineCap.Round;
+				if(object.ReferenceEquals(value, ag.Paint.Cap.Square))
+					return PenLineCap.Square;
+				throw new NotSupportedException();
+		}
 	}
 }
 
--- Source/Eto.Platform.Android/Drawing/PenHandler.cs
+++ Source/Eto.Platform.Android/Drawing/PenHandler.cs
@@ -14,7 +14,88 @@ using ag = Android.Graphics;
 
 namespace Eto.Platform.Android.Drawing
 {
-	class PenHandler
+	/// <summary>
+	/// Handler for <see cref="IPen"/>
+	/// </summary>
+	/// <copyright>(c) 2013 by Vivek Jhaveri</copyright>
+	/// <license type="BSD-3">See LICENSE for full terms</license>
+	public class PenHandler : IPen
 	{
+		public object Create(Color color, float thickness)
+		{
+			var paint = new ag.Paint 
+			{ 
+				Color = color.ToAndroid(), 
+				StrokeWidth = thickness,
+				StrokeCap = ag.Paint.Cap.Square,
+				StrokeMiter = 10f
+			};
+			return paint;
+		}
+
+		public Color GetColor(Pen widget)
+		{
+			return widget.ToAndroid().Color.ToEto();
+		}
+
+		public void SetColor(Pen widget, Color color)
+		{
+			widget.ToAndroid().Color = color.ToAndroid();
+		}
+
+		public float GetThickness(Pen widget)
+		{
+			return widget.ToAndroid().StrokeWidth;
+		}
+
+		public void SetThickness(Pen widget, float thickness)
+		{
+			widget.ToAndroid().StrokeWidth = thickness;
+		}
+
+		public PenLineJoin GetLineJoin(Pen widget)
+		{
+			return widget.ToAndroid().StrokeJoin.ToEto();
+		}
+
+		public void SetLineJoin(Pen widget, PenLineJoin lineJoin)
+		{
+			widget.ToAndroid().StrokeJoin = lineJoin.ToAndroid();
+		}
+
+		public PenLineCap GetLineCap(Pen widget)
+		{
+			return widget.ToAndroid().StrokeCap.ToEto();
+		}
+
+		public void SetLineCap(Pen widget, PenLineCap lineCap)
+		{
+			var pen = widget.ToAndroid();
+			pen.StrokeCap = lineCap.ToSD();
+			SetDashStyle(widget, widget.DashStyle);
+		}
+
+		public float GetMiterLimit(Pen widget)
+		{
+			return widget.ToAndroid().StrokeMiter;
+		}
+
+		public void SetMiterLimit(Pen widget, float miterLimit)
+		{
+			widget.ToAndroid().StrokeMiter = miterLimit;
+		}
+
+		public void SetDashStyle(Pen widget, DashStyle dashStyle)
+		{
+			var pen = widget.ToAndroid();
+
+			if (dashStyle == null || dashStyle.IsSolid)
+				pen.SetPathEffect(null);
+			else
+			{
+				// TODO: create a new ag.DashPathEffect with the appropriate intervals
+				throw new NotImplementedException();
+			}
+		}
 	}
 }
