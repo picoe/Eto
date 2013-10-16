3af1e6500d660bd36c0d6e7793b90a7dda360c0a
Curtis Wensley <curtis.wensley@gmail.com>
2013-10-15 17:18:57 -0700

    Mac: Call Form.OnShown after window is shown (to be consistent with dialog)

--- Source/Eto.Platform.Mac/Forms/FormHandler.cs
+++ Source/Eto.Platform.Mac/Forms/FormHandler.cs
@@ -1,32 +1,30 @@
 using System;
-using Eto.Drawing;
 using Eto.Forms;
 using MonoMac.AppKit;
 using SD = System.Drawing;
 
 namespace Eto.Platform.Mac.Forms
 {
-	public class FormHandler : MacWindow<MyWindow, Form>, IDisposable, IForm
+	public class FormHandler : MacWindow<MyWindow, Form>, IForm
 	{
 		protected override bool DisposeControl { get { return false; } }
 
 		public FormHandler()
 		{
-			Control = new MyWindow(new SD.Rectangle(0,0,200,200), 
-				NSWindowStyle.Resizable | NSWindowStyle.Closable | NSWindowStyle.Miniaturizable | NSWindowStyle.Titled, 
-				NSBackingStore.Buffered, false);
-			ConfigureWindow ();
+			Control = new MyWindow(new SD.Rectangle(0, 0, 200, 200), 
+			                       NSWindowStyle.Resizable | NSWindowStyle.Closable | NSWindowStyle.Miniaturizable | NSWindowStyle.Titled, 
+			                       NSBackingStore.Buffered, false);
+			ConfigureWindow();
 		}
-		
 
-		public void Show ()
+		public void Show()
 		{
-			if (!Control.IsVisible)
-				Widget.OnShown (EventArgs.Empty);
-			if (this.WindowState == WindowState.Minimized)
-				Control.MakeKeyWindow ();
+			if (WindowState == WindowState.Minimized)
+				Control.MakeKeyWindow();
 			else
-				Control.MakeKeyAndOrderFront (ApplicationHandler.Instance.AppDelegate);
+				Control.MakeKeyAndOrderFront(ApplicationHandler.Instance.AppDelegate);
+			if (!Control.IsVisible)
+				Widget.OnShown(EventArgs.Empty);
 		}
 	}
 }
