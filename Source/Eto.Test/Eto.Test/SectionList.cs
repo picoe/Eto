00bea044a3a9f31a86f14500475a1c9d778e7abc
Curtis Wensley <curtis.wensley@gmail.com>
2013-11-22 11:24:28 -0800

    Fix Direct2D platform compilation

--- Source/Eto.Test/Eto.Test/SectionList.cs
+++ Source/Eto.Test/Eto.Test/SectionList.cs
@@ -165,7 +165,6 @@ namespace Eto.Test
 		}
 	}
 
-#if Windows
 	public class D2DToolkit : DrawingToolkit
 	{
 		Graphics graphics;
@@ -197,6 +196,5 @@ namespace Eto.Test
 			return new D2DToolkit();
 		}
 	}
-#endif
 }
 
