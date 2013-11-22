a0057b494c4440432693324976808106f86739c1
vivek <vivekjhaveri@hotmail.com>
2013-11-22 22:31:12 +0530

    Create separate section for Direct2D tests.

--- Source/Eto.Test/Eto.Test/TestSectionList.cs
+++ Source/Eto.Test/Eto.Test/TestSectionList.cs
@@ -12,6 +12,9 @@ namespace Eto.Test
 			yield return new Section("Forms", FormsSection());
 			yield return new Section("Behaviors", BehaviorsSection());
 			yield return new Section("Drawing", DrawingSection());
+#if Windows
+			yield return new Section("Drawing using Direct2D on a Winforms Control", DrawingSectionDirect2D());
+#endif
 			yield return new Section("Controls", ControlSection());
 			yield return new Section("Layouts", LayoutsSection());
 			yield return new Section("Dialogs", DialogsSection());
@@ -69,16 +72,31 @@ namespace Eto.Test
 			yield return new Section<ClearSection> { Text = "Clear" };
 			yield return new Section<DrawTextSection> { Text = "Draw Text" };
 			yield return new Section<DirectDrawingSection> { Text = "Direct Drawing" };
-#if Windows
-			var usingd2d = " using Direct2D on a Winforms Control";
-
-			//yield return new Section<BitmapSection> { Text = "Bitmap" + usingd2d, Creator = () => new BitmapSection(new D2DToolkit()) };
-			yield return new Section<DrawTextSection> { Text = "Draw Text" + usingd2d, Creator = () => new DrawTextSection(new D2DToolkit()) };
-			yield return new Section<DirectDrawingSection> { Text = "Direct Drawing" + usingd2d, Creator = () => new DirectDrawingSection(new D2DToolkit()) };
-			yield return new Section<GraphicsPathSection> { Text = "Graphics Path" + usingd2d, Creator = () => new GraphicsPathSection(new D2DToolkit()) };
-#endif
 			yield return new Section<UnitTestSection> { Text = "Unit Tests" };
 		}
+
+#if Windows
+		static IEnumerable<Section> DrawingSectionDirect2D()
+		{
+			// yield return new Section<BitmapSection> { Text = "Bitmap" + usingd2d, Creator = () => new BitmapSection(new D2DToolkit()) };
+			// yield return new Section<IndexedBitmapSection> { Text = "Indexed Bitmap" };
+			yield return new Section<GraphicsPathSection> { Text = "Graphics Path", Creator = () => new GraphicsPathSection(new D2DToolkit()) };
+			// yield return new Section<AntialiasSection> { Text = "Antialias" };
+			// yield return new Section<FontsSection> { Text = "Control Fonts" };
+			// yield return new Section<InterpolationSection> { Text = "Image Interpolation" };
+			// yield return new Section<PenSection> { Text = "Pens" };
+			// yield return new Section<PixelOffsetSection> { Text = "Pixel Offset" };
+			// yield return new Section<TransformSection> { Text = "Transform" };
+			// yield return new Section<BrushSection> { Text = "Brushes" };
+			// yield return new Section<TextureBrushesSection> { Text = "Texture Brushes" };
+			// yield return new Section<TextureBrushesSection2> { Text = "Texture Brushes 2" };
+			// yield return new Section<ClipSection> { Text = "Clip" };
+			// yield return new Section<ClearSection> { Text = "Clear" };
+			yield return new Section<DrawTextSection> { Text = "Draw Text", Creator = () => new DrawTextSection(new D2DToolkit()) };
+			yield return new Section<DirectDrawingSection> { Text = "Direct Drawing", Creator = () => new DirectDrawingSection(new D2DToolkit()) };
+			// yield return new Section<UnitTestSection> { Text = "Unit Tests" };
+		}
+#endif
 		
 		static IEnumerable<Section> LayoutsSection()
 		{
