fe0951860057c4dd3140b633025cc52c0f8fbc27
vivek <vivekjhaveri@hotmail.com>
2013-11-26 14:50:17 +0530

    Use foreach instead of the ForEach() extension. See http://stackoverflow.com/a/10299492/90291

--- Source/Eto/Forms/Controls/RadioButtonList.cs
+++ Source/Eto/Forms/Controls/RadioButtonList.cs
@@ -245,7 +245,7 @@ namespace Eto.Forms
 			if (force || changed) {
 				selectedButton = button;
 				settingChecked = true;
-				buttons.ForEach (r => r.Checked = object.ReferenceEquals (r, button));
+				foreach(var r in buttons) r.Checked = object.ReferenceEquals (r, button);
 				settingChecked = false;
 				if (sendEvent && changed && Loaded)
 					OnSelectedIndexChanged (EventArgs.Empty);
