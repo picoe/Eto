82586896fe2e900341cd068c7a693d08f540ce8b
vivek <vivekjhaveri@hotmail.com>
2013-12-02 14:30:50 +0530

    Fix texture names on iOS.

--- Source/Eto.Test/Eto.Test/TestIcons.cs
+++ Source/Eto.Test/Eto.Test/TestIcons.cs
@@ -1,20 +1,30 @@
+using System.Reflection;
 using Eto.Drawing;
 
 namespace Eto.Test
 {
 	public static class TestIcons
 	{
-		public static string TestIconName = "Eto.Test.TestIcon.ico"; // static not const so test apps can override
+		static string prefix;
+		static string Prefix { get { return prefix = prefix ?? Assembly.GetExecutingAssembly().GetName().Name + "."; } }
 
+#if IOS
+		// Resources on iOS are stored without the extension
+		public static string TestIconName = "TestIcon"; 
+		public static string TestImageName = "TestImage";
+		public static string TexturesName = "Textures";
+#else
+		public static string TestIconName = "TestIcon.ico"; 
+		public static string TestImageName = "TestImage.png";
+		public static string TexturesName = "Textures.png";
+#endif
 		static Icon testIcon;
-		public static Icon TestIcon { get { return testIcon = testIcon ?? Icon.FromResource(TestIconName); } }
+		public static Icon TestIcon { get { return testIcon = testIcon ?? Icon.FromResource(Prefix + TestIconName); } }
 
-		public static string TestImageName = "Eto.Test.TestImage.png"; // static not const so test apps can override
 		static Bitmap testImage;
-		public static Bitmap TestImage { get { return testImage = testImage ?? Bitmap.FromResource(TestImageName); } }
+		public static Bitmap TestImage { get { return testImage = testImage ?? Bitmap.FromResource(Prefix + TestImageName); } }
 
-		public static string TexturesName = "Eto.Test.Textures.png"; // static not const so test apps can override
 		static Bitmap textures;
-		public static Bitmap Textures { get { return textures = textures ?? Bitmap.FromResource(TexturesName); } }
+		public static Bitmap Textures { get { return textures = textures ?? Bitmap.FromResource(Prefix + TexturesName); } }
 	}
 }
