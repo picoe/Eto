b1b0381d2282a408f0b16e94bc9caf2912595ec9
vivek <vivekjhaveri@hotmail.com>
2013-12-02 23:05:35 +0530

    Add texture brush test shapes.

--- Source/Eto.Test/Eto.Test/Sections/Drawing/TextureBrushesSection2.cs
+++ Source/Eto.Test/Eto.Test/Sections/Drawing/TextureBrushesSection2.cs
@@ -22,13 +22,34 @@ namespace Eto.Test.Sections.Drawing
 				location = e.Location;
 				drawable.Invalidate(); };
 			drawable.Paint += (s, e) => {
-				e.Graphics.DrawText(font, Colors.White, 3, 3, "Move the mouse in this area to move the image.");
+				e.Graphics.DrawText(font, Colors.White, 3, 3, "Move the mouse in this area to move the textured shapes.");
 
 				var temp = brush.Transform; // save state					
 				brush.Transform = Matrix.FromTranslation(location);
 				e.Graphics.FillRectangle(brush, new RectangleF(location, img.Size));
 				brush.Transform = temp;
+
+				location += new PointF(0, 100);
+				temp = brush.Transform; // save state					
+				brush.Transform = Matrix.FromTranslation(location);
+				e.Graphics.FillEllipse(brush, new RectangleF(location, img.Size));
+				brush.Transform = temp;
+
+				location += new PointF(0, 100);
+				temp = brush.Transform; // save state					
+				brush.Transform = Matrix.FromTranslation(location);
+				var polygon = GetPolygon(location);
+				e.Graphics.FillPolygon(brush, polygon);
+				brush.Transform = temp;
 			};
+		}
+
+		private static PointF[] GetPolygon(PointF location)
+		{
+			var polygon = new PointF[] { new PointF(0, 50), new PointF(50, 100), new PointF(100, 50), new PointF(50, 0) };
+			for (var i = 0; i < polygon.Length; ++i)
+				polygon[i] += location;
+			return polygon;
 		}
 	}
 }
