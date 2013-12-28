77564e51e2ab978c9b79ad2175961f2202b434a8
Curtis Wensley <curtis.wensley@gmail.com>
2013-12-27 17:00:30 -0800

    Gtk: Reset all invokes when application loop is finished so all threads have a chance to exit properly

--- Source/Eto.Platform.Gtk/Forms/ApplicationHandler.cs
+++ Source/Eto.Platform.Gtk/Forms/ApplicationHandler.cs
@@ -13,6 +13,7 @@ namespace Eto.Platform.GtkSharp
 	{
 		bool attached;
 		Gtk.StatusIcon statusIcon;
+		readonly List<ManualResetEvent> invokeResetEvents = new List<ManualResetEvent>();
 
 		protected override void Initialize()
 		{
@@ -21,15 +22,15 @@ namespace Eto.Platform.GtkSharp
 		}
 
 		public static int MainThreadID { get; set; }
-		
-		public void RunIteration ()
+
+		public void RunIteration()
 		{
-			Gtk.Application.RunIteration ();
+			Gtk.Application.RunIteration();
 		}
 
-		public void Restart ()
+		public void Restart()
 		{
-			Gtk.Application.Quit ();
+			Gtk.Application.Quit();
 
 			// TODO: restart!
 		}
@@ -42,20 +43,23 @@ namespace Eto.Platform.GtkSharp
 			set
 			{
 				badgeLabel = value;
-				if (!string.IsNullOrEmpty (badgeLabel))
+				if (!string.IsNullOrEmpty(badgeLabel))
 				{
-					if (statusIcon == null) {
-						statusIcon = new Gtk.StatusIcon ();
-						statusIcon.Activate += delegate {
+					if (statusIcon == null)
+					{
+						statusIcon = new Gtk.StatusIcon();
+						statusIcon.Activate += delegate
+						{
 							var form = Application.Instance.MainForm;
 							if (form != null)
-								form.BringToFront ();
+								form.BringToFront();
 						};
 					}
 					var bmp = new Gdk.Pixbuf(Gdk.Colorspace.Rgb, true, 8, 32, 32);
-					using (var graphics = new Graphics(new Bitmap (Widget.Generator, new BitmapHandler (bmp)))) {
-						graphics.Clear ();
-						DrawBadgeLabel (graphics, new Size (bmp.Width, bmp.Height), badgeLabel);
+					using (var graphics = new Graphics(new Bitmap(Widget.Generator, new BitmapHandler(bmp))))
+					{
+						graphics.Clear();
+						DrawBadgeLabel(graphics, new Size(bmp.Width, bmp.Height), badgeLabel);
 					}
 					statusIcon.Pixbuf = bmp;
 					statusIcon.Visible = true;
@@ -65,27 +69,31 @@ namespace Eto.Platform.GtkSharp
 			}
 		}
 
-		protected virtual void DrawBadgeLabel (Graphics graphics, Size size, string badgeLabel)
+		protected virtual void DrawBadgeLabel(Graphics graphics, Size size, string badgeLabel)
 		{
-			var rect = new Rectangle (size);
-			rect.Inflate (-2, -2);
-			graphics.FillEllipse (Brushes.Red (Generator), rect);
-			graphics.DrawEllipse (new Pen(Colors.White, 2, Generator), rect);
+			var rect = new Rectangle(size);
+			rect.Inflate(-2, -2);
+			graphics.FillEllipse(Brushes.Red(Generator), rect);
+			graphics.DrawEllipse(new Pen(Colors.White, 2, Generator), rect);
 			var font = new Font(SystemFont.Bold, 10);
-			var labelSize = graphics.MeasureString (font, badgeLabel);
+			var labelSize = graphics.MeasureString(font, badgeLabel);
 			var labelPosition = ((PointF)(rect.Size - labelSize) / 2) + rect.Location;
-			graphics.DrawText (font, Colors.White, labelPosition, badgeLabel);
-			graphics.Flush ();
+			graphics.DrawText(font, Colors.White, labelPosition, badgeLabel);
+			graphics.Flush();
 		}
 
-		public void Invoke (Action action)
+		public void Invoke(Action action)
 		{
 			if (Thread.CurrentThread.ManagedThreadId == ApplicationHandler.MainThreadID)
-				action ();
-			else {
-				var resetEvent = new ManualResetEvent (false);
+				action();
+			else
+			{
+				var resetEvent = new ManualResetEvent(false);
+				lock (invokeResetEvents)
+					invokeResetEvents.Add(resetEvent);
 
-				Gtk.Application.Invoke (delegate {
+				Gtk.Application.Invoke(delegate
+				{
 					try
 					{
 						action();
@@ -95,15 +103,17 @@ namespace Eto.Platform.GtkSharp
 						resetEvent.Set();
 					}
 				});
-
-				resetEvent.WaitOne ();
+				resetEvent.WaitOne();
+				lock (invokeResetEvents)
+					invokeResetEvents.Remove(resetEvent);
 			}
 		}
 
-		public void AsyncInvoke (Action action)
+		public void AsyncInvoke(Action action)
 		{
-			Gtk.Application.Invoke (delegate {
-				action ();
+			Gtk.Application.Invoke(delegate
+			{
+				action();
 			});
 		}
 
@@ -116,32 +126,41 @@ namespace Eto.Platform.GtkSharp
 		{
 		}
 
-		public void Run (string[] args)
+		public void Run(string[] args)
 		{
 			//if (!Platform.IsWindows) Gdk.Threads.Init(); // do this in windows, and it stalls!  ugh
 			MainThreadID = Thread.CurrentThread.ManagedThreadId;
-			
-			Widget.OnInitialized (EventArgs.Empty);
+
+			Widget.OnInitialized(EventArgs.Empty);
 			if (!attached)
 			{
 				Gtk.Application.Run();
+				lock (invokeResetEvents)
+				{
+					// set all invokes so threads will continue when run loop exits
+					foreach (var reset in invokeResetEvents)
+						reset.Set();
+					invokeResetEvents.Clear();
+				}
+
 				Gdk.Threads.Leave();
 			}
 		}
 
-		public override void AttachEvent (string id)
+		public override void AttachEvent(string id)
 		{
-			switch (id) {
-			case Application.TerminatingEvent:
+			switch (id)
+			{
+				case Application.TerminatingEvent:
 				// called automatically
-				break;
-			default:
-				base.AttachEvent (id);
-				break;
+					break;
+				default:
+					base.AttachEvent(id);
+					break;
 			}
 		}
 
-		public void Quit ()
+		public void Quit()
 		{
 			var args = new CancelEventArgs();
 			var mainForm = Widget.MainForm != null ? Widget.MainForm.Handler as IGtkWindow : null;
@@ -153,11 +172,11 @@ namespace Eto.Platform.GtkSharp
 			if (!args.Cancel)
 				Gtk.Application.Quit();
 		}
-		
-		public void Open (string url)
+
+		public void Open(string url)
 		{
-			var info = new ProcessStartInfo (url);
-			Process.Start (info);
+			var info = new ProcessStartInfo(url);
+			Process.Start(info);
 		}
 
 		public IEnumerable<Command> GetSystemCommands()
@@ -169,16 +188,8 @@ namespace Eto.Platform.GtkSharp
 		{
 		}
 
-		public Keys CommonModifier {
-			get {
-				return Keys.Control;
-			}
-		}
+		public Keys CommonModifier { get { return Keys.Control; } }
 
-		public Keys AlternateModifier {
-			get {
-				return Keys.Alt;
-			}
-		}
+		public Keys AlternateModifier { get { return Keys.Alt; } }
 	}
 }
