59408aa6bb5eb76a77304e4e1ead5b042dc9901b
Curtis Wensley <curtis.wensley@gmail.com>
2014-01-20 09:40:25 -0800

    Gtk: Update BaseComboBox to use gtk arrow-size style to determine the width of the drop down arrow

--- Source/Eto.Platform.Gtk/CustomControls/BaseComboBox.cs
+++ Source/Eto.Platform.Gtk/CustomControls/BaseComboBox.cs
@@ -7,66 +7,50 @@ namespace Eto.Platform.GtkSharp.CustomControls
 	{
 		Entry entry;
 		Button popupButton;
-		
-		public BaseComboBox ()
+
+		public BaseComboBox()
 		{
 			AppPaintable = true;
-			Build ();
+			Build();
 #if GTK3
-			//popupButton.AppPaintable = true;
-			//popupButton.Drawn += popup_Drawn;
 			SetSizeRequest(150, 30);
 			foreach (var cls in PopupButton.StyleContext.ListClasses())
 				PopupButton.StyleContext.RemoveClass(cls);
-			foreach (var cls in Entry.StyleContext.ListClasses())
-			{
-				//Console.WriteLine(cls);
-				//Entry.StyleContext.RemoveClass(cls);
-			}
-
-
 #endif
-			//popupButton.Visible = false;
-			//popupButton.NoShowAll = true;
-
-			//entry.AppPaintable = true;
-			//popupButton.Drawn += (o, args) => {
-			//	args.RetVal = false;
-			//};
 		}
-
-#if GTK2
+		#if GTK2
 		[GLib.ConnectBefore]
-		protected override bool OnExposeEvent (Gdk.EventExpose evnt)
+		protected override bool OnExposeEvent(Gdk.EventExpose evnt)
 		{
 			var rect = Allocation;
 			
-			if (rect.Width > 0 && rect.Height > 0) {
-				//Gtk.Style.PaintFlatBox (Entry.Style, this.GdkWindow, Entry.State, Gtk.ShadowType.In, evnt.Area, this, "entry_bg", rect.X, rect.Y, rect.Width, rect.Height);
-				Gtk.Style.PaintShadow (Entry.Style, GdkWindow, Entry.State, ShadowType.In, evnt.Area, Entry, "entry", rect.X, rect.Y, rect.Width, rect.Height);
+			if (rect.Width > 0 && rect.Height > 0)
+			{
+				Gtk.Style.PaintShadow(Entry.Style, GdkWindow, Entry.State, ShadowType.In, evnt.Area, Entry, "entry", rect.X, rect.Y, rect.Width, rect.Height);
 				var arrowWidth = popupButton.Allocation.Width;
-				var arrowPos = rect.Right - arrowWidth - 4;
+				var arrowPos = rect.Right - arrowWidth - 1;
 				Gtk.Style.PaintArrow(Entry.Style, GdkWindow, Entry.State, ShadowType.None, evnt.Area, this, "arrow", ArrowType.Down, true, arrowPos, rect.Top, arrowWidth, rect.Height);
 				Gtk.Style.PaintVline(Entry.Style, GdkWindow, Entry.State, evnt.Area, this, "line", rect.Top + 4, rect.Bottom - 4, arrowPos - 1);
 			}
 			return true;
 		}
+		
 #else
-		//[GLib.ConnectBefore]
-		protected override bool OnDrawn (Cairo.Context cr)
+		protected override bool OnDrawn(Cairo.Context cr)
 		{
 			bool ret = true;
 			var rect = this.Allocation;
-			if (rect.Width > 0 && rect.Height > 0) {
+			if (rect.Width > 0 && rect.Height > 0)
+			{
 				var arrowWidth = popupButton.Allocation.Width;
-				var arrowPos = rect.Width - arrowWidth + 4;
+				var arrowPos = rect.Width - arrowWidth;
 				var arrowSize = 10;
 
-				StyleContext.Save ();
-                StyleContext.AddClass ("entry");
-                StyleContext.RenderBackground (cr, 0, 0, rect.Width, rect.Height);
+				StyleContext.Save();
+				StyleContext.AddClass("entry");
+				StyleContext.RenderBackground(cr, 0, 0, rect.Width, rect.Height);
 
-				ret = base.OnDrawn (cr);
+				ret = base.OnDrawn(cr);
 
 				StyleContext.RenderArrow(cr, Math.PI, arrowPos, (rect.Height - arrowSize) / 2, arrowSize);
 
@@ -74,8 +58,8 @@ namespace Eto.Platform.GtkSharp.CustomControls
 				cr.Rectangle(arrowPos - 5, 2, 1, rect.Height - 4);
 				cr.Fill();
 
-				Entry.StyleContext.RenderFrame (cr, 0, 0, rect.Width, rect.Height);
-                StyleContext.Restore ();
+				Entry.StyleContext.RenderFrame(cr, 0, 0, rect.Width, rect.Height);
+				StyleContext.Restore();
 
 			}
 			return ret;
@@ -94,32 +78,35 @@ namespace Eto.Platform.GtkSharp.CustomControls
 			}
 		}
 
-#endif
-
+		#endif
 		public Entry Entry { get { return entry; } }
 
 		public Button PopupButton { get { return popupButton; } }
-		
-		Gtk.Widget CreateEntry ()
+
+		Gtk.Widget CreateEntry()
 		{
 			entry = new Entry
 			{
 				CanFocus = true,
 				IsEditable = true,
-				HasFrame = false
+				HasFrame = false,
 			};
 
-			entry.FocusInEvent += delegate {
-				QueueDraw ();
+			entry.FocusInEvent += delegate
+			{
+				QueueDraw();
 			};
 			
-			entry.FocusOutEvent += delegate {
-				QueueDraw ();
+			entry.FocusOutEvent += delegate
+			{
+				QueueDraw();
 			};
 			return entry;
 		}
-		
-		Gtk.Widget CreatePopupButton ()
+
+		static readonly int ArrowSize = Convert.ToInt32(new ComboBox().StyleGetProperty("arrow-size"));
+
+		Gtk.Widget CreatePopupButton()
 		{
 #if GTK3
 			popupButton = new InvisibleButton();
@@ -127,29 +114,28 @@ namespace Eto.Platform.GtkSharp.CustomControls
 
 			popupButton = new Button();
 #endif
-			popupButton.WidthRequest = 25;
+			popupButton.WidthRequest = ArrowSize + 6;
 			popupButton.CanFocus = false;
 			return popupButton;
 		}
-        
-		void Build ()
+
+		void Build()
 		{
 			var vbox = new VBox();
-			var hbox = new HBox ();
+			var hbox = new HBox();
 
 #if GTK2
-			hbox.PackStart (CreateEntry (), true, true, 2);
-			hbox.PackEnd (CreatePopupButton (), true, false, 2);
-			vbox.PackStart(hbox, true, true, 4);
+			hbox.PackStart(CreateEntry(), true, true, 4);
+			hbox.PackEnd(CreatePopupButton(), false, false, 2);
+			vbox.PackStart(hbox, true, true, 2);
 #else
-			hbox.PackStart (CreateEntry (), true, true, 1);
-			hbox.PackEnd (CreatePopupButton (), true, false, 0);
-			vbox.PackStart(hbox, true, true, 1);
+			hbox.PackStart(CreateEntry(), true, true, 0);
+			hbox.PackEnd(CreatePopupButton(), false, false, 2);
+			vbox.PackStart(hbox, true, true, 0);
 #endif
 
 			Add(vbox);
 		}
-		
 	}
 }
 
