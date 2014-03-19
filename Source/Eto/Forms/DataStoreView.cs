28c94909d62d9c9cc44e70c8dfa1503b8c56ceaf
vivek <vivekjhaveri@hotmail.com>
2014-03-19 17:35:22 +0530

    DataStoreView fix: do not sort if SortComparer is null.

--- Source/Eto/Forms/DataStoreView.cs
+++ Source/Eto/Forms/DataStoreView.cs
@@ -175,7 +175,9 @@ namespace Eto.Forms
 			public Comparison<object> SortComparer { get; set; }
 			public int Compare(object x, object y)
 			{
-				return SortComparer == null ? 0 : SortComparer(x, y);
+				if (SortComparer == null)
+					throw new InvalidOperationException("SortComparer must not be null");
+				return SortComparer(x, y);
 			}
 		}
 
@@ -197,9 +199,9 @@ namespace Eto.Forms
 				var viewItems = (Filter != null) ? temp.Where(Filter).ToList() : temp.ToList();
 
 				// sort if needed
-				if (comparer != null)
+				if (comparer != null &&
+					comparer.SortComparer != null)
 					viewItems.Sort(comparer);
-
 
 				// If a sort or filter is specified, create a dictionary
 				// of the item indices. This materializes a list of all the
