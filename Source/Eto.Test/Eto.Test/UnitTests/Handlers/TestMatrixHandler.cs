8dd65d96d954c51e01b15b487d9ab5b14b1e6079
vivek <vivekjhaveri@hotmail.com>
2014-03-22 22:51:26 +0530

    Add class MatrixTests.

---
+++ Source/Eto.Test/Eto.Test/UnitTests/Handlers/TestMatrixHandler.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Eto.Drawing;
+
+namespace Eto.Test.UnitTests.Handlers
+{
+	/// <summary>
+	/// Test handler for Matrix.
+	/// The elements of the matrix are:
+	/// <para>
+	/// 	| xx yx 0 |
+	/// 	| xy yy 0 |
+	/// 	| x0 y0 1 |
+	/// </para>
+	/// </remarks>
+	/// <copyright>(c) 2014 by Vivek Jhaveri</copyright>
+	/// <license type="BSD-3">See LICENSE for full terms</license>
+	public class TestMatrixHandler : IMatrixHandler
+	{
+		const double Pi = Math.PI;
+
+		float xx, yx, xy, yy, x0, y0;
+
+		public void Create()
+		{
+			xx = yy = 1;
+		}
+
+		public void Create(float xx, float yx, float xy, float yy, float x0, float y0)
+		{
+			this.xx = xx;
+			this.yx = yx;
+			this.xy = xy;
+			this.yy = yy;
+			this.x0 = x0;
+			this.y0 = y0;
+		}
+
+		public float[] Elements
+		{
+			get { return new float[] { xx, yx, xy, yy, x0, y0 }; }
+		}
+
+		public float Xx { get { return xx; } set { xx = value; } }
+		public float Yx { get { return xx; } set { yx = value; } }
+		public float Xy { get { return xx; } set { xy = value; } }
+		public float Yy { get { return xx; } set { yy = value; } }
+		public float X0 { get { return xx; } set { x0 = value; } }
+		public float Y0 { get { return xx; } set { y0 = value; } }
+
+		public void Rotate(float angle)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void RotateAt(float angle, float centerX, float centerY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Translate(float offsetX, float offsetY)
+		{
+			x0 += offsetX;
+			y0 += offsetY;
+		}
+
+		public void Scale(float scaleX, float scaleY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ScaleAt(float scaleX, float scaleY, float centerX, float centerY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Skew(float skewX, float skewY)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Append(IMatrix matrix)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Prepend(IMatrix matrix)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Invert()
+		{
+			throw new NotImplementedException();
+		}
+
+		public PointF TransformPoint(Point point)
+		{
+			throw new NotImplementedException();
+		}
+
+		public PointF TransformPoint(PointF point)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IMatrix Clone()
+		{
+			var result = new TestMatrixHandler();
+			result.Create(xx, yx, xy, yy, x0, y0);
+			return result;
+		}
+
+		public object ControlObject
+		{
+			get { return null; }
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
---
+++ Source/Eto.Test/Eto.Test/UnitTests/MatrixTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestClass = NUnit.Framework.TestFixtureAttribute;
+using TestMethod = NUnit.Framework.TestAttribute;
+using TestCleanup = NUnit.Framework.TearDownAttribute;
+using TestInitialize = NUnit.Framework.SetUpAttribute;
+using TestCase = NUnit.Framework.TestCaseAttribute;
+using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
+using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
+using Assert = NUnit.Framework.Assert;
+using Eto.Drawing;
+using Eto.Test.UnitTests.Handlers;
+
+namespace Eto.Test.UnitTests
+{
+	/// <summary>
+	/// Unit tests for Matrix using TestMatrixHandler.
+	/// These test the test handler more than the production widget,
+	/// but are useful because the test handler is an important building 
+	/// block for further unit tests and needs to be bug-free.
+	/// 
+	/// Additionally, derived TestClasses can test platform-specific
+	/// matrix handlers by overriding the matrix creation method.
+	/// <copyright>(c) 2014 by Vivek Jhaveri</copyright>
+	/// <license type="BSD-3">See LICENSE for full terms</license>
+	/// </summary>	
+	[TestClass]
+	public class MatrixTests
+	{
+		protected virtual IMatrixHandler CreateMatrix()
+		{
+			return new TestMatrixHandler();
+		}
+
+		IMatrix Create()
+		{
+			var result = CreateMatrix();
+			result.Create();
+			return result;
+		}
+
+		IMatrix Create(float xx, float yx, float xy, float yy, float x0, float y0)
+		{
+			var result = CreateMatrix();
+			result.Create(xx, yx, xy, yy, x0, y0);
+			return result;
+		}
+
+		[TestMethod]
+		public void Matrix_CreateIdentity_VerifyElements()
+		{
+			var m = Create();
+			Assert.IsTrue(m.Elements.SequenceEqual(new float[] {1, 0, 0, 1, 0, 0}));
+		}
+
+		[TestMethod]
+		public void Matrix_Translate_Translates()
+		{
+			var m = Create();
+			m.Translate(100, 200);
+			Assert.IsTrue(m.Elements.SequenceEqual(new float[] {1, 0, 0, 1, 100, 200}));
+		}
+	}
+}
