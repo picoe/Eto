fc4bfa11c30cc549e8826f2d0b579a9e02ee0934
vivek <vivekjhaveri@hotmail.com>
2014-04-01 00:16:32 +0530

    Remove NUnit namespace mappings.

--- Source/Eto.Test/Eto.Test.WinForms/UnitTests/MatrixTests.cs
+++ Source/Eto.Test/Eto.Test.WinForms/UnitTests/MatrixTests.cs
@@ -2,19 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using TestClass = NUnit.Framework.TestFixtureAttribute;
-using TestMethod = NUnit.Framework.TestAttribute;
-using TestCleanup = NUnit.Framework.TearDownAttribute;
-using TestInitialize = NUnit.Framework.SetUpAttribute;
-using TestCase = NUnit.Framework.TestCaseAttribute;
-using ClassCleanup = NUnit.Framework.TestFixtureTearDownAttribute;
-using ClassInitialize = NUnit.Framework.TestFixtureSetUpAttribute;
-using Assert = NUnit.Framework.Assert;
+using NUnit.Framework;
 using Eto.Drawing;
 
 namespace Eto.Test.WinForms.UnitTests
 {
-	[TestClass]
+	[TestFixture]
 	public class MatrixTests : Eto.Test.UnitTests.MatrixTests
 	{
 		protected override IMatrixHandler CreateMatrix()
