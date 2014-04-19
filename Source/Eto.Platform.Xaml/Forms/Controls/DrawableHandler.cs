ec07b9c5abd22ab843f5234243842db5ee49d478
vivek <vivekjhaveri@hotmail.com>
2014-04-19 19:58:29 +0530

    Use WriteableBitmap to copy pixel data. This does not currently work, probably because the raw data values need to be written, whereas currently PNG data is being written.

--- Source/Eto.Platform.Xaml/Forms/Controls/DrawableHandler.cs
+++ Source/Eto.Platform.Xaml/Forms/Controls/DrawableHandler.cs
@@ -135,16 +135,12 @@ namespace Eto.Platform.Xaml.Forms.Controls
 			var graphicsHandler = graphics.Handler as GraphicsHandler;
 			var bitmap = graphicsHandler.Image;
 			var bitmapHandler = bitmap.Handler as BitmapHandler;
-			var memoryStream = new System.IO.MemoryStream();
-			bitmapHandler.Save(memoryStream, ImageFormat.Png);  // save the wic bitmap to the memoryStream
-			var bytes = memoryStream.ToArray(); // hurray, we've got the bytes
-
-			//await SaveToFile(bitmapHandler.Control);
-
-			var stream = new MemoryRandomAccessStream(bytes); // TODO: does adding a using() work here?
-			var image = new BitmapImage();
-			await image.SetSourceAsync(stream);
-			Control.Source = image;
+			var bitmapObject = bitmapHandler.Control;
+			var size = bitmapObject.Size;
+			var writeableBitmap = new WriteableBitmap(size.Width, size.Height);
+			var stream = writeableBitmap.PixelBuffer.AsStream();
+			bitmapHandler.Save(stream, ImageFormat.Png);  // save the wic bitmap to the memoryStream
+			Control.Source = writeableBitmap;
 		}
 
 		async Task SaveToFile(SharpDX.WIC.Bitmap bitmap)
@@ -174,94 +170,6 @@ namespace Eto.Platform.Xaml.Forms.Controls
 									 96, 96, bytes);
 
 				await encoder.FlushAsync();
-			}
-		}
-
-		/// <summary>
-		/// See http://stackoverflow.com/a/12571315
-		/// </summary>
-		class MemoryRandomAccessStream : IRandomAccessStream
-		{
-			private Stream m_InternalStream;
-
-			public MemoryRandomAccessStream(Stream stream)
-			{
-				this.m_InternalStream = stream;
-			}
-
-			public MemoryRandomAccessStream(byte[] bytes)
-			{
-				this.m_InternalStream = new MemoryStream(bytes);
-			}
-
-			public IInputStream GetInputStreamAt(ulong position)
-			{
-				this.m_InternalStream.Seek((long)position, SeekOrigin.Begin);
-
-
-				return this.m_InternalStream.AsInputStream();
-			}
-
-			public IOutputStream GetOutputStreamAt(ulong position)
-			{
-				this.m_InternalStream.Seek((long)position, SeekOrigin.Begin);
-
-
-				return this.m_InternalStream.AsOutputStream();
-			}
-
-			public ulong Size
-			{
-				get { return (ulong)this.m_InternalStream.Length; }
-				set { this.m_InternalStream.SetLength((long)value); }
-			}
-
-			public bool CanRead
-			{
-				get { return true; }
-			}
-
-			public bool CanWrite
-			{
-				get { return true; }
-			}
-
-			public IRandomAccessStream CloneStream()
-			{
-				throw new NotSupportedException();
-			}
-
-			public ulong Position
-			{
-				get { return (ulong)this.m_InternalStream.Position; }
-			}
-
-			public void Seek(ulong position)
-			{
-				this.m_InternalStream.Seek((long)position, 0);
-			}
-
-			public void Dispose()
-			{
-				this.m_InternalStream.Dispose();
-			}
-
-			public Windows.Foundation.IAsyncOperationWithProgress<IBuffer, uint> ReadAsync(IBuffer buffer, uint count, InputStreamOptions options)
-			{
-				var inputStream = this.GetInputStreamAt(0);
-				return inputStream.ReadAsync(buffer, count, options);
-			}
-
-			public Windows.Foundation.IAsyncOperation<bool> FlushAsync()
-			{
-				var outputStream = this.GetOutputStreamAt(0);
-				return outputStream.FlushAsync();
-			}
-
-			public Windows.Foundation.IAsyncOperationWithProgress<uint, uint> WriteAsync(IBuffer buffer)
-			{
-				var outputStream = this.GetOutputStreamAt(0);
-				return outputStream.WriteAsync(buffer);
 			}
 		}
 
