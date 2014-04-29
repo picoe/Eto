334b504c04eb5775ca3a58b4400c54910ee63779
Curtis Wensley <curtis.wensley@gmail.com>
2014-04-29 09:46:53 -0700

    Remove incomplete DrawableCell for release

--- Source/Eto.Platform.Windows/Generator.cs
+++ Source/Eto.Platform.Windows/Generator.cs
@@ -46,8 +46,7 @@ namespace Eto.Platform.Windows
 			g.Add<IImageTextCell>(() => new ImageTextCellHandler());
 			g.Add<IImageViewCell>(() => new ImageViewCellHandler());
 			g.Add<ITextBoxCell>(() => new TextBoxCellHandler());
-			g.Add<IDrawableCell>(() => new DrawableCellHandler());
-			
+
 			// Forms.Controls
 			g.Add<IButton>(() => new ButtonHandler());
 			g.Add<ICheckBox>(() => new CheckBoxHandler());
