56e27f01d72f7c4a786fbdd570143dba1b590396
Curtis Wensley <curtis.wensley@gmail.com>
2014-08-05 22:36:06 -0700

    Use better title for Mac/Mac64/XamMac assemblies/nuget packages

--- Source/Eto.Mac/Properties/AssemblyInfo.cs
+++ Source/Eto.Mac/Properties/AssemblyInfo.cs
@@ -1,10 +1,13 @@
 using System.Reflection;
 
 #if XAMMAC
-[assembly: AssemblyTitle("Eto.Forms - OS X Platform using Xamarin.Mac")]
+[assembly: AssemblyTitle("Eto.Forms - Xamarin.Mac Platform")]
 [assembly: AssemblyDescription("OS X Platform for the Eto.Forms UI Framework using Xamarin.Mac")]
-#else
-[assembly: AssemblyTitle("Eto.Forms - OS X Platform using MonoMac")]
+#elif Mac64
+[assembly: AssemblyTitle("Eto.Forms - MonoMac Platform")]
 [assembly: AssemblyDescription("OS X Platform for the Eto.Forms UI Framework using the open-source MonoMac")]
+#else
+[assembly: AssemblyTitle("Eto.Forms - MonoMac 64-bit Platform")]
+[assembly: AssemblyDescription("OS X Platform for the Eto.Forms UI Framework using the open-source MonoMac with 64-bit mono")]
 #endif
 
