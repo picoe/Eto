6ec5e44f4148c2bebac48638df974cec8747b8bc
Curtis Wensley <curtis.wensley@gmail.com>
2014-11-04 21:25:56 -0800

    Rename solutions with net45 and net40 suffixes, and roll WinRT sln into net45

--- Resources/package.cmd
+++ Resources/package.cmd
@@ -1,6 +1,7 @@
 echo off
 
-set MSBUILD=c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+rem set MSBUILD=c:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
+set MSBUILD="%ProgramFiles(x86)%\MSBuild\12.0\bin\MSBuild.exe"
 
 %MSBUILD% -t:Package -p:BuildVersion=%1 Publish.targets
 
