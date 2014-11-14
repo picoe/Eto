79720b40e01895d9ba582cc9354b7af9c192c791
Curtis Wensley <curtis.wensley@gmail.com>
2014-11-13 19:12:05 -0800

    Update version to 2.0.0.0, file/nuget version 2.0.1. Skipping 2.0.0 due to incorrect version of the development nuget feed.

--- Source/Shared/GlobalAssemblyInfo.cs
+++ Source/Shared/GlobalAssemblyInfo.cs
@@ -6,5 +6,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.99.*")]
-[assembly: AssemblyInformationalVersion("1.99.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyInformationalVersion("2.0.1")]
