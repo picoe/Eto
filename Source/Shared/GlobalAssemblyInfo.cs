a2026157252d771a4a53e5b42c2e4c18f87ade26
Curtis Wensley <curtis.wensley@gmail.com>
2014-11-15 22:55:54 -0800

    Update to 2.0.3

--- Source/Shared/GlobalAssemblyInfo.cs
+++ Source/Shared/GlobalAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.2.0")]
-[assembly: AssemblyInformationalVersion("2.0.2")]
+[assembly: AssemblyFileVersion("2.0.3.0")]
+[assembly: AssemblyInformationalVersion("2.0.3")]
