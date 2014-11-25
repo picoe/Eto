e2b6e1a84437314bb9224022e7b54790d70ff4e6
Curtis Wensley <curtis.wensley@gmail.com>
2014-11-24 18:52:12 -0800

    Compile XS addin for 5.5.0+ Add build-addin script

--- Source/Addins/Eto.Addin.XamarinStudio/Properties/AddinInfo.cs
+++ Source/Addins/Eto.Addin.XamarinStudio/Properties/AddinInfo.cs
@@ -13,5 +13,5 @@ using Mono.Addins.Description;
 [assembly:AddinDescription("Addin to easily start developing with Eto.Forms using project and file templates.")]
 [assembly:AddinAuthor("Curtis Wensley")]
 
-[assembly:AddinDependency("::MonoDevelop.Core", MonoDevelop.BuildInfo.Version)]
-[assembly:AddinDependency("::MonoDevelop.Ide", MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency("::MonoDevelop.Core", "5.5.0")] //MonoDevelop.BuildInfo.Version)]
+[assembly:AddinDependency("::MonoDevelop.Ide", "5.5.0")] //, MonoDevelop.BuildInfo.Version)]
