6186b77f24fd9eb585bf42a4f5039b8eca397757
Curtis Wensley <curtis.wensley@gmail.com>
2014-11-29 01:40:58 -0800

    Update ColumnBinding to enable get values from arrays/lists that are not of the same type, and convert the value to the destination type if needed.

--- Source/Eto/Binding/ColumnBinding.cs
+++ Source/Eto/Binding/ColumnBinding.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Globalization;
 
 namespace Eto
 {
@@ -15,16 +17,16 @@ namespace Eto
 		/// </summary>
 		/// <param name="column">column/index to get the value</param>
 		/// <returns>value of the object with the specified column/index</returns>
-		object GetValue (int column);
+		object GetValue(int column);
 
 		/// <summary>
 		/// Sets the value of this object for the specified column/index
 		/// </summary>
 		/// <param name="column">column/index to set the value</param>
 		/// <param name="value">value to set at the specified column/index</param>
-		void SetValue (int column, object value);
+		void SetValue(int column, object value);
 	}
-	
+
 	/// <summary>
 	/// Column/Index binding for objects implementing <see cref="IColumnItem"/> or <see cref="IList"/>
 	/// </summary>
@@ -41,53 +43,74 @@ namespace Eto
 		/// Gets or sets the column the binding will get/set the value
 		/// </summary>
 		public int Column { get; set; }
-		
+
 		/// <summary>
 		/// Initializes a new instance of the ColumnBinding class
 		/// </summary>
-		public ColumnBinding ()
+		public ColumnBinding()
 		{
 		}
-		
+
 		/// <summary>
 		/// Initializes a new instance of the ColumnBinding class with the specified column
 		/// </summary>
 		/// <param name="column">column/index to get/set the value from each object</param>
-		public ColumnBinding (int column)
+		public ColumnBinding(int column)
 		{
 			this.Column = column;
 		}
-		
+
 		/// <summary>
 		/// Implements the logic to get the value from the specified object
 		/// </summary>
 		/// <param name="dataItem">object to get the value from</param>
 		/// <returns>value at the <see cref="Column"/> of the specified object</returns>
-		protected override T InternalGetValue (object dataItem)
+		protected override T InternalGetValue(object dataItem)
 		{
+			var list = dataItem as IList;
+			if (list != null)
+				return Convert(list[Column]);
+
 			var colitem = dataItem as IColumnItem;
-			if (colitem != null) {
-				return (T)colitem.GetValue (Column);
-			}
+			if (colitem != null)
+				return Convert(colitem.GetValue(Column));
+
 			var listitem = dataItem as IList<T>;
-			return listitem == null ? default(T) : listitem[Column];
+			if (listitem != null)
+				return listitem[Column];
+
+			return default(T);
 		}
-		
+
+		static T Convert(object val)
+		{
+			var propertyType = typeof(T);
+			if (val != null && !propertyType.IsInstanceOfType(val))
+			{
+				propertyType = Nullable.GetUnderlyingType(propertyType) ?? propertyType;
+				val = System.Convert.ChangeType(val, propertyType, CultureInfo.InvariantCulture);
+			}
+			return (T)val;
+		}
+
 		/// <summary>
 		/// Implements the logic to set the value to the specified object
 		/// </summary>
 		/// <param name="dataItem">object to set the value</param>
 		/// <param name="value">value to set at the <see cref="Column"/> of the specified object</param>
-		protected override void InternalSetValue (object dataItem, T value)
+		protected override void InternalSetValue(object dataItem, T value)
 		{
+			var list = dataItem as IList;
+			if (list != null)
+				list[Column] = value;
+
 			var colitem = dataItem as IColumnItem;
-			if (colitem != null) {
-				colitem.SetValue (Column, value);
-			}
-			var listitem = dataItem as IList;
-			if (listitem != null) {
-				listitem [Column] = value;
-			}
+			if (colitem != null)
+				colitem.SetValue(Column, value);
+
+			var listitem = dataItem as IList<T>;
+			if (listitem != null)
+				listitem[Column] = value;
 		}
 	}
 }
