d5ef3990bcb4bf33ab0e9580c3f231a9faaa5aa9
Curtis Wensley <curtis.wensley@gmail.com>
2015-01-19 20:44:22 -0800

    Wpf: DateTimePicker Fixes: - Only show calendar in popup when Mode = Date - Only show spinner when Mode = Time (no need for popup)

--- Source/Eto.Wpf/Forms/Controls/DateTimePickerHandler.cs
+++ Source/Eto.Wpf/Forms/Controls/DateTimePickerHandler.cs
@@ -2,13 +2,19 @@ using System;
 using Eto.Forms;
 using mwc = Xceed.Wpf.Toolkit;
 using sw = System.Windows;
+using swc = System.Windows.Controls;
 using System.Globalization;
 using Eto.Drawing;
+using Eto.Wpf.Drawing;
 
 namespace Eto.Wpf.Forms.Controls
 {
-	public class DateTimePickerHandler : WpfControl<mwc.DateTimePicker, DateTimePicker, DateTimePicker.ICallback>, DateTimePicker.IHandler
+	public class DateTimePickerHandler : WpfFrameworkElement<swc.Border, DateTimePicker, DateTimePicker.ICallback>, DateTimePicker.IHandler
 	{
+		mwc.DateTimePicker dtp;
+		mwc.DateTimeUpDown dtud;
+		DateTime? last;
+
 		DateTimePickerMode mode;
 
 		protected override Size DefaultSize { get { return new Size(mode == DateTimePickerMode.DateTime ? 180 : 120, -1); } }
@@ -17,36 +23,87 @@ namespace Eto.Wpf.Forms.Controls
 
 		public DateTimePickerHandler()
 		{
-			Control = new mwc.DateTimePicker
-			{
-				ShowButtonSpinner = false,
-				AutoCloseCalendar = true,
-				ClipValueToMinMax = true,
-				Focusable = true,
-				IsTabStop = true
-			};
+			Control = new swc.Border { Focusable = false };
 			#pragma warning disable 612,618
 			Mode = DateTimePicker.DefaultMode;
 			#pragma warning restore 612,618
-			DateTime? last = Value;
-			Control.ValueChanged += delegate
-			{
-				var val = Value;
-				if (val != null)
-				{
-					// still need to do this as the popup doesn't limit the value when using the calendar
-					if (Control.Minimum != null && val.Value < Control.Minimum)
-						val = Value = Control.Minimum.Value;
-					else if (Control.Maximum != null && val.Value > Control.Maximum)
-						val = Value = Control.Maximum.Value;
-				}
+		}
 
-				if (last != val && (last == null || val == null || Math.Abs((last.Value - val.Value).TotalSeconds) >= 1))
+		void CreateDateTimeUpDown()
+		{
+			if (dtud == null)
+			{
+				dtud = new mwc.DateTimeUpDown
 				{
-					Callback.OnValueChanged(Widget, EventArgs.Empty);
-					last = val;
-				}
-			};
+					Focusable = true,
+					IsTabStop = true,
+					ClipValueToMinMax = true,
+					ShowButtonSpinner = true
+				};
+				dtud.ValueChanged += UpDown_ValueChanged;
+			}
+			if (dtp != null)
+			{
+				CopyValues(dtp, dtud);
+				dtp.ValueChanged -= UpDown_ValueChanged;
+				dtp = null;
+			}
+			Control.Child = dtud;
+		}
+
+		void CreateDateTimePicker()
+		{
+			if (dtp == null)
+			{
+				dtp = new mwc.DateTimePicker
+				{
+					ShowButtonSpinner = false,
+					AutoCloseCalendar = true,
+					ClipValueToMinMax = true,
+					Focusable = true,
+					IsTabStop = true
+				};
+				dtp.ValueChanged += UpDown_ValueChanged;
+			}
+			if (dtud != null)
+			{
+				CopyValues(dtud, dtp);
+				dtud.ValueChanged -= UpDown_ValueChanged;
+				dtud = null;
+			}
+			Control.Child = dtp;
+		}
+
+		void CopyValues(mwc.Primitives.UpDownBase<DateTime?> source, mwc.Primitives.UpDownBase<DateTime?> dest)
+		{
+			dest.Minimum = source.Minimum;
+			dest.Maximum = source.Maximum;
+			dest.Value = source.Value;
+			dest.Background = source.Background;
+			dest.Foreground = source.Foreground;
+			var font = Widget.Properties.Get<Font>(FontKey);
+			if (font != null)
+				dest.SetEtoFont(font, SetDecorations);
+		}
+
+		void UpDown_ValueChanged(object sender, sw.RoutedPropertyChangedEventArgs<object> e)
+		{
+			var ctl = UpDown;
+			var val = Value;
+			if (val != null)
+			{
+				// still need to do this as the popup doesn't limit the value when using the calendar
+				if (ctl.Minimum != null && val.Value < ctl.Minimum)
+					val = Value = ctl.Minimum.Value;
+				else if (ctl.Maximum != null && val.Value > ctl.Maximum)
+					val = Value = ctl.Maximum.Value;
+			}
+
+			if (last != val && (last == null || val == null || Math.Abs((last.Value - val.Value).TotalSeconds) >= 1))
+			{
+				Callback.OnValueChanged(Widget, EventArgs.Empty);
+				last = val;
+			}
 		}
 
 		public override bool UseMousePreview { get { return true; } }
@@ -55,20 +112,28 @@ namespace Eto.Wpf.Forms.Controls
 
 		public DateTime? Value
 		{
-			get { return Control.Value; }
-			set { Control.Value = value; }
+			get { return UpDown.Value; }
+			set { UpDown.Value = value; }
+		}
+
+		mwc.Primitives.UpDownBase<DateTime?> UpDown
+		{
+			get
+			{
+				return dtp ?? dtud;
+			}
 		}
 
 		public DateTime MinDate
 		{
-			get { return Control.Minimum ?? DateTime.MinValue; }
-			set { Control.Minimum = value == DateTime.MinValue ? null : (DateTime?)value; }
+			get { return UpDown.Minimum ?? DateTime.MinValue; }
+			set { UpDown.Minimum = value == DateTime.MinValue ? null : (DateTime?)value; }
 		}
 
 		public DateTime MaxDate
 		{
-			get { return Control.Maximum ?? DateTime.MaxValue; }
-			set { Control.Maximum = value == DateTime.MaxValue ? null : (DateTime?)value; }
+			get { return UpDown.Maximum ?? DateTime.MaxValue; }
+			set { UpDown.Maximum = value == DateTime.MaxValue ? null : (DateTime?)value; }
 		}
 
 		public DateTimePickerMode Mode
@@ -80,21 +145,57 @@ namespace Eto.Wpf.Forms.Controls
 				switch (mode)
 				{
 					case DateTimePickerMode.Date:
-						Control.Format = mwc.DateTimeFormat.ShortDate;
+						CreateDateTimePicker();
+						dtp.TimePickerVisibility = sw.Visibility.Collapsed;
+						dtp.Format = mwc.DateTimeFormat.ShortDate;
 						break;
 					case DateTimePickerMode.DateTime:
+						CreateDateTimePicker();
+						dtp.TimePickerVisibility = sw.Visibility.Visible;
 						var format = CultureInfo.CurrentUICulture.DateTimeFormat;
-						Control.Format = mwc.DateTimeFormat.Custom;
-						Control.FormatString = format.ShortDatePattern + " " + format.LongTimePattern;
+						dtp.Format = mwc.DateTimeFormat.Custom;
+						dtp.FormatString = format.ShortDatePattern + " " + format.LongTimePattern;
 						break;
 					case DateTimePickerMode.Time:
-						Control.Format = mwc.DateTimeFormat.LongTime;
+						CreateDateTimeUpDown();
+						dtud.Format = mwc.DateTimeFormat.LongTime;
 						break;
 					default:
 						throw new NotSupportedException();
 				}
 				SetSize();
 			}
+		}
+
+		public override Color BackgroundColor
+		{
+			get { return UpDown.Background.ToEtoColor(); }
+			set { UpDown.Background = value.ToWpfBrush(Control.Background); }
+		}
+
+		protected virtual void SetDecorations(sw.TextDecorationCollection decorations)
+		{
+		}
+
+		static readonly object FontKey = new object();
+
+		public Font Font
+		{
+			get { return Widget.Properties.Create<Font>(FontKey, () => new Font(new FontHandler(UpDown))); }
+			set
+			{
+				if (Widget.Properties.Get<Font>(FontKey) != value)
+				{
+					Widget.Properties[FontKey] = value;
+					UpDown.SetEtoFont(value, SetDecorations);
+				}
+			}
+		}
+
+		public Color TextColor
+		{
+			get { return UpDown.Foreground.ToEtoColor(); }
+			set { UpDown.Foreground = value.ToWpfBrush(UpDown.Foreground); }
 		}
 	}
 }
