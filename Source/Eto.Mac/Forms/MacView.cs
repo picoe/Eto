0fb8cec83435b24175f61efe8213930a3bf7e63c
Curtis Wensley <curtis.wensley@gmail.com>
2015-05-01 11:54:58 -0700

    Use full framework for XamMac2 Mac: Fix compile warning

--- Source/Eto.Mac/Forms/MacView.cs
+++ Source/Eto.Mac/Forms/MacView.cs
@@ -751,7 +751,6 @@ namespace Eto.Mac.Forms
 		}
 
 		Dictionary<IntPtr, Command> systemActions;
-		static readonly IntPtr selRespondsToSelector = Selector.GetHandle("respondsToSelector:");
 		static readonly IntPtr selGetAction = Selector.GetHandle("action");
 		static readonly IntPtr selValidateUserInterfaceItem = Selector.GetHandle("validateUserInterfaceItem:");
 		static readonly IntPtr selCut = Selector.GetHandle("cut:");
