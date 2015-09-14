6cec01ba42531aa6d8c2d05dbdb130cfed6412ae
Curtis Wensley <curtis.wensley@gmail.com>
2015-09-13 22:02:11 -0700

    Update to v2.2

--- Source/Shared/GlobalAssemblyInfo.cs
+++ Source/Shared/GlobalAssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("2.1.0.0")]
-//[assembly: AssemblyFileVersion("2.1.0")]
-[assembly: AssemblyInformationalVersion("2.1.0")]
+[assembly: AssemblyVersion("2.1.*")]
+//[assembly: AssemblyFileVersion("2.2.0")]
+[assembly: AssemblyInformationalVersion("2.2.0-alpha")]
