af2175ad5497b4eca7b5554b75cb4a03d5c683c0
Curtis Wensley <curtis.wensley@gmail.com>
2015-10-10 02:33:30 -0700

    Add script to make a new release and update version numbers.

--- Source/Shared/GlobalAssemblyInfo.cs
+++ Source/Shared/GlobalAssemblyInfo.cs
@@ -7,5 +7,4 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 [assembly: AssemblyVersion("2.1.*")]
-//[assembly: AssemblyFileVersion("2.2.0")]
 [assembly: AssemblyInformationalVersion("2.2.0-alpha")]
