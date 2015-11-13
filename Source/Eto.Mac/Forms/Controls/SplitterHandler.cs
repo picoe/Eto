picoe/Eto

commit 1f83505c5e0055af21cc74c6a651fa5fbd3d70cb

Fix up Splitter for all platforms.
- When Panel1 or Panel2 is null or Visible is set to false, then associated panel(s) are collapsed automatically. Fixes #380.
- Better deal with FixedPanel and Orientation changes at runtime and keep the same split position.
- Better handle unloading and loading the same instance of the control and retaining the correct splitter position.

Source/Eto.Mac/Forms/Controls/SplitterHandler.cs
@@ -33,11 +33,44 @@ using nuint = System.UInt32;
 
 namespace Eto.Mac.Forms.Controls
 {
-	public class SplitterHandler : MacView<NSSplitView, Splitter, Splitter.ICallback>, Splitter.IHandler
+	public class SplitterHandler : MacView<NSSplitView, Splitter, Splitter.ICallback>, Splitter.IHandler, IMacContainer
 	{
+		public void SetContentSize(CGSize contentSize)
+		{
+		}
+
+		public void LayoutParent(bool updateSize = true)
+		{
+			Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+		}
+
+		public void LayoutChildren()
+		{
+		}
+
+		public void LayoutAllChildren()
+		{
+		}
+
+		public bool InitialLayout
+		{
+			get { return false; }
+		}
+
 		double relative = double.NaN;
 
-		//TODO: RelativePosition - following is just stub, see WinForm/WPF/GTK or ThemedSplitter
+		double GetRelativePosition()
+		{
+			var pos = Position;
+			if (fixedPanel == SplitterFixedPanel.Panel1)
+				return pos;
+			var size = Orientation == Orientation.Horizontal ? Control.Bounds.Width : Control.Bounds.Height;
+			size -= SplitterWidth;
+			if (fixedPanel == SplitterFixedPanel.Panel2)
+				return size - pos;
+			return pos / (double)size;
+		}
+
 		public double RelativePosition
 		{
 			get
@@ -45,14 +78,7 @@ namespace Eto.Mac.Forms.Controls
 				if (!Widget.Loaded)
 					return relative;
 				
-				var pos = Position;
-				if (fixedPanel == SplitterFixedPanel.Panel1)
-					return pos;
-				var size = Orientation == Orientation.Horizontal ? Control.Bounds.Width : Control.Bounds.Height;
-				size -= SplitterWidth;
-				if (fixedPanel == SplitterFixedPanel.Panel2)
-					return size - pos;
-				return pos / (double)size;
+				return GetRelativePosition();
 			}
 			set
 			{
@@ -87,7 +113,12 @@ namespace Eto.Mac.Forms.Controls
 		public int SplitterWidth
 		{
 			get { return (int)Math.Round(Control.DividerThickness); }
-			set { }
+			set {
+				if (value <= 2)
+					Control.DividerStyle = NSSplitViewDividerStyle.Thin;
+				else
+					Control.DividerStyle = NSSplitViewDividerStyle.Thick;
+			}
 		}
 
 
@@ -96,6 +127,13 @@ namespace Eto.Mac.Forms.Controls
 		int? position;
 		SplitterFixedPanel fixedPanel;
 		bool initialPositionSet;
+		static readonly object WasLoaded_Key = new object();
+
+		bool WasLoaded
+		{ 
+			get { return Widget.Properties.Get<bool>(WasLoaded_Key); } 
+			set { Widget.Properties.Set(WasLoaded_Key, value); }
+		}
 
 		public bool RecurseToChildren { get { return true; } }
 
@@ -120,9 +158,26 @@ namespace Eto.Mac.Forms.Controls
 		{
 			var splitView = handler.Control;
 			var dividerThickness = splitView.DividerThickness;
+			var newFrame = splitView.Frame;
+
+			if (handler.panel1 == null || !handler.panel1.Visible)
+			{
+				splitView.Subviews[0].SetFrameSize(SizeF.Empty.ToNS());
+				splitView.Subviews[1].Frame = newFrame;
+				return;
+			}
+
+			if (handler.panel2 == null || !handler.panel2.Visible)
+			{
+				splitView.Subviews[0].Frame = newFrame;
+				splitView.Subviews[1].SetFrameSize(SizeF.Empty.ToNS());
+				return;
+			}
+			if (handler.initialPositionSet)
+				handler.SetRelative();
+
 			var panel1Rect = splitView.Subviews[0].Frame;
 			var panel2Rect = splitView.Subviews[1].Frame;
-			var newFrame = splitView.Frame;
 				
 			if (oldSize.Height <= 0 && oldSize.Width <= 0)
 				oldSize = newFrame.Size;
@@ -214,11 +269,19 @@ namespace Eto.Mac.Forms.Controls
 
 			public override void DidResizeSubviews(NSNotification notification)
 			{
-				var subview = Handler.Control.Subviews[0];
-				if (subview != null && Handler.position != null && Handler.initialPositionSet && Handler.Widget.Loaded && Handler.Widget.ParentWindow != null && Handler.Widget.ParentWindow.Loaded)
+				var h = Handler;
+				var subview = h.Control.Subviews[0];
+				if (subview != null && h.position != null && h.initialPositionSet && h.Widget.Loaded && h.Widget.ParentWindow != null && h.Widget.ParentWindow.Loaded)
 				{
-					Handler.position = Handler.Control.IsVertical ? (int)subview.Frame.Width : (int)subview.Frame.Height;
-					Handler.Callback.OnPositionChanged(Handler.Widget, EventArgs.Empty);
+					if (h.panel1 == null || !h.panel1.Visible || h.panel2 == null || !h.panel2.Visible)
+					{
+						// remember relative position if either panel is not visible
+						if (double.IsNaN(h.relative))
+							h.relative = h.RelativePosition;
+						return;
+					}
+					h.position = h.Control.IsVertical ? (int)subview.Frame.Width : (int)subview.Frame.Height;
+					h.Callback.OnPositionChanged(h.Widget, EventArgs.Empty);
 				}
 			}
 		}
@@ -314,7 +377,15 @@ namespace Eto.Mac.Forms.Controls
 			{
 				fixedPanel = value;
 				if (Widget.Loaded)
+				{
+					if (double.IsNaN(relative))
+						relative = RelativePosition;
 					Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+				}
+				else if (WasLoaded)
+				{
+					relative = GetRelativePosition();
+				}
 			}
 		}
 
@@ -388,6 +459,7 @@ namespace Eto.Mac.Forms.Controls
 		public override void OnLoadComplete(EventArgs e)
 		{
 			base.OnLoadComplete(e);
+			WasLoaded = false;
 			SetInitialSplitPosition();
 			Control.ResizeSubviewsWithOldSize(CGSize.Empty);
 			initialPositionSet = true;
@@ -396,6 +468,10 @@ namespace Eto.Mac.Forms.Controls
 		public override void OnUnLoad(EventArgs e)
 		{
 			base.OnUnLoad(e);
+			WasLoaded = true;
+			// remember relative position if we're shown again at a different size
+			if (double.IsNaN(relative))
+				relative = GetRelativePosition();
 			initialPositionSet = false;
 		}
 

Source/Eto.Gtk/Forms/Controls/SplitterHandler.cs
@@ -33,8 +33,7 @@ namespace Eto.GtkSharp.Forms.Controls
 					else
 					{
 						// both get at least the preferred size
-						return (int)Math.Round(Math.Max(
-							width1 / relative, width2 / (1 - relative)));
+						return (int)Math.Round(Math.Max(width1 / relative, width2 / (1 - relative))) + SplitterWidth;
 					}
 				}
 			}
@@ -269,8 +268,17 @@ namespace Eto.GtkSharp.Forms.Controls
 				if (fixedPanel != value)
 				{
 					fixedPanel = value;
+					var position = Position;
+					if (WasLoaded)
+						UpdateRelative();
+
 					((Gtk.Paned.PanedChild)Control[Control.Child1]).Resize = value != SplitterFixedPanel.Panel1;
 					((Gtk.Paned.PanedChild)Control[Control.Child2]).Resize = value != SplitterFixedPanel.Panel2;
+
+					if (Control.IsRealized)
+						SetPosition(position);
+					else if (WasLoaded)
+						SetRelative(relative);
 				}
 			}
 		}
@@ -295,9 +303,17 @@ namespace Eto.GtkSharp.Forms.Controls
 				Control = new EtoHPaned() { Handler = this };
 			else
 				Control = new EtoVPaned() { Handler = this };
+
+			Control.ShowAll();
+			Control.Realized += (sender, e) =>
+			{
+				SetInitialPosition();
+				HookEvents();
+			};
+
 			if (container.Child != null)
 				container.Remove(container.Child);
-			container.Child = Control;
+
 			if (old != null)
 			{
 				var child1 = old.Child1;
@@ -313,11 +329,8 @@ namespace Eto.GtkSharp.Forms.Controls
 				Control.Pack1(EmptyContainer(), fixedPanel != SplitterFixedPanel.Panel1, true);
 				Control.Pack2(EmptyContainer(), fixedPanel != SplitterFixedPanel.Panel2, true);
 			}
-			Control.Realized += (sender, e) =>
-			{
-				SetInitialPosition();
-				HookEvents();
-			};
+
+			container.Child = Control;
 		}
 
 		void HookEvents()
@@ -347,6 +360,7 @@ namespace Eto.GtkSharp.Forms.Controls
 		public override void OnLoad(EventArgs e)
 		{
 			base.OnLoad(e);
+			WasLoaded = false;
 			suppressSplitterMoved++;
 			if (Control.IsRealized)
 				SetInitialPosition();
@@ -356,6 +370,22 @@ namespace Eto.GtkSharp.Forms.Controls
 		{
 			base.OnLoadComplete(e);
 			suppressSplitterMoved--;
+		}
+
+		static readonly object WasLoaded_Key = new object();
+
+		bool WasLoaded
+		{
+			get { return Widget.Properties.Get<bool>(WasLoaded_Key); }
+			set { Widget.Properties.Set(WasLoaded_Key, value); }
+		}
+
+		public override void OnUnLoad(EventArgs e)
+		{
+			base.OnUnLoad(e);
+			WasLoaded = true;
+			position = null;
+			relative = RelativePosition;
 		}
 
 		void SetInitialPosition()

Source/Eto.WinForms/Forms/Controls/SplitterHandler.cs
@@ -16,6 +16,7 @@ namespace Eto.WinForms.Forms.Controls
 
 		public bool RecurseToChildren { get { return true; } }
 
+
 		public override Size? DefaultSize
 		{
 			get
@@ -23,10 +24,7 @@ namespace Eto.WinForms.Forms.Controls
 				var size = base.DefaultSize;
 				if (size == null && Control.AutoSize)
 				{
-					var min = Control.MinimumSize;
-					Control.MinimumSize = sd.Size.Empty;
-					size = Control.GetPreferredSize(sd.Size.Empty).ToEto();
-					Control.MinimumSize = min;
+					size = GetPreferredSize().ToEto();
 				}
 				return size;
 			}
@@ -55,14 +53,13 @@ namespace Eto.WinForms.Forms.Controls
 				it.SetRelative(it.relative);
 				it.suppressSplitterMoved--;
 			}
-
 		}
 
-		sd.Size GetPreferredSize()
+		sd.Size GetPreferredSize(bool limit = false)
 		{
 			var size = new sd.Size();
-			var size1 = panel1 == null ? Size.Empty : panel1.GetPreferredSize();
-			var size2 = panel2 == null ? Size.Empty : panel2.GetPreferredSize();
+			var size1 = panel1 == null || !panel1.Visible ? Size.Empty : panel1.GetPreferredSize();
+			var size2 = panel2 == null || !panel2.Visible ? Size.Empty : panel2.GetPreferredSize();
 			if (Orientation == Orientation.Horizontal)
 			{
 				if (position.HasValue)
@@ -81,6 +78,8 @@ namespace Eto.WinForms.Forms.Controls
 					{
 						// both get at least the preferred size
 						size1.Width = (int)Math.Round(Math.Max(size1.Width/relative, size2.Width/(1-relative)));
+						if (Widget.Loaded && Control.IsHandleCreated)
+							size1.Width = Math.Min(Control.Width - SplitterWidth, size1.Width);
 						size2.Width = 0;
 					}
 				}
@@ -105,6 +104,8 @@ namespace Eto.WinForms.Forms.Controls
 					{
 						// both get at least the preferred size
 						size1.Height = (int)Math.Round(Math.Max(size1.Height/relative, size2.Height/(1-relative)));
+						if (Widget.Loaded && Control.IsHandleCreated)
+							size1.Height = Math.Min(Control.Height - SplitterWidth, size1.Height);
 						size2.Height = 0;
 					}
 				}
@@ -184,13 +185,11 @@ namespace Eto.WinForms.Forms.Controls
 			if (desired)
 			{
 				var size = UserDesiredSize;
-				var pick = Orientation == Orientation.Horizontal ?
-					size.Width : size.Height;
+				var pick = Orientation == Orientation.Horizontal ? size.Width : size.Height;
 				if (pick >= 0)
 					return pick - Control.SplitterWidth;
 			}
-			return (Orientation == Orientation.Horizontal ?
-				Control.Width : Control.Height) - Control.SplitterWidth;
+			return (Orientation == Orientation.Horizontal ? Control.Width : Control.Height) - Control.SplitterWidth;
 		}
 
 		void UpdateRelative()
@@ -276,13 +275,18 @@ namespace Eto.WinForms.Forms.Controls
 			suppressSplitterMoved--;
 		}
 
+		void SetCollapsed()
+		{
+			Control.Panel1Collapsed = !Panel1Visible;
+			Control.Panel2Collapsed = !Panel2Visible;
+		}
+
 		void SetInitialPosition()
 		{
 			suppressSplitterMoved++;
 			try
 			{
-				Control.Panel1Collapsed = panel1 == null || !(panel1.GetWindowsHandler()).InternalVisible;
-				Control.Panel2Collapsed = panel2 == null || !(panel2.GetWindowsHandler()).InternalVisible;
+				SetCollapsed();
 				if (position != null)
 				{
 					var pos = position.Value;
@@ -341,7 +345,10 @@ namespace Eto.WinForms.Forms.Controls
 			{
 				fixedPanel = value;
 				if (Control.IsHandleCreated)
+				{
 					SetFixedPanel();
+					UpdateRelative();
+				}
 			}
 		}
 
@@ -420,7 +427,7 @@ namespace Eto.WinForms.Forms.Controls
 					}
 					if (Widget.Loaded)
 					{
-						Control.Panel1Collapsed = panel1 == null || !(panel1.GetWindowsHandler()).InternalVisible;
+						Control.Panel1Collapsed = Panel1Visible;
 						Control.ResumeLayout();
 					}
 				}
@@ -454,27 +461,49 @@ namespace Eto.WinForms.Forms.Controls
 					}
 					if (Widget.Loaded)
 					{
-						Control.Panel2Collapsed = panel2 == null || !(panel2.GetWindowsHandler()).InternalVisible;
+						Control.Panel2Collapsed = Panel2Visible;
 						Control.ResumeLayout();
 					}
 				}
 			}
 		}
 
+		bool Panel1Visible
+		{
+			get { return panel1 != null && panel1.GetWindowsHandler().InternalVisible; }
+		}
+
+		bool Panel2Visible
+		{
+			get { return panel2 != null && panel2.GetWindowsHandler().InternalVisible; }
+		}
+
 		void c1_VisibleChanged(object sender, EventArgs e)
 		{
-			if (panel1 != null && (panel1.GetWindowsHandler()).InternalVisible)
-				Control.Panel1Collapsed = false;
-			else
-				Control.Panel1Collapsed = true;
+			VisibleChanged(Panel1Visible);
 		}
 
 		void c2_VisibleChanged(object sender, EventArgs e)
 		{
-			if (panel2 != null && (panel2.GetWindowsHandler()).InternalVisible)
-				Control.Panel2Collapsed = false;
-			else
-				Control.Panel2Collapsed = true;
+			VisibleChanged(Panel2Visible);
 		}
+
+		void VisibleChanged(bool isVisible)
+		{
+			if (isVisible)
+			{
+				SetCollapsed();
+				if (!double.IsNaN(relative))
+					SetRelative(relative);
+			}
+			else
+			{
+				position = null;
+				if (double.IsNaN(relative))
+					UpdateRelative();
+				SetCollapsed();
+			}
+		}
+
 	}
 }

commit 6e19f725067e70d1ba9c09190ea472fbbb62d7f8

Mac: Fix regression when updating layouts inside a splitter caused by 1f83505c5e0055af21cc74c6a651fa5fbd3d70cb

Source/Eto.Mac/Forms/Controls/SplitterHandler.cs
@@ -33,28 +33,13 @@ using nuint = System.UInt32;
 
 namespace Eto.Mac.Forms.Controls
 {
-	public class SplitterHandler : MacView<NSSplitView, Splitter, Splitter.ICallback>, Splitter.IHandler, IMacContainer
+	public class SplitterHandler : MacContainer<NSSplitView, Splitter, Splitter.ICallback>, Splitter.IHandler
 	{
-		public void SetContentSize(CGSize contentSize)
-		{
-		}
 
-		public void LayoutParent(bool updateSize = true)
+		public override void LayoutParent(bool updateSize = true)
 		{
-			Control.ResizeSubviewsWithOldSize(CGSize.Empty);
-		}
-
-		public void LayoutChildren()
-		{
-		}
-
-		public void LayoutAllChildren()
-		{
-		}
-
-		public bool InitialLayout
-		{
-			get { return false; }
+			UpdatePosition();
+			base.LayoutParent(updateSize);
 		}
 
 		double relative = double.NaN;
@@ -86,7 +71,7 @@ namespace Eto.Mac.Forms.Controls
 				if (Widget.Loaded)
 				{
 					SetRelative();
-					Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+					UpdatePosition();
 				}
 			}
 		}
@@ -350,7 +335,7 @@ namespace Eto.Mac.Forms.Controls
 				position = value;
 				relative = double.NaN;
 				if (Widget.Loaded)
-					Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+					UpdatePosition();
 			}
 		}
 
@@ -364,7 +349,7 @@ namespace Eto.Mac.Forms.Controls
 			{
 				Control.IsVertical = value == Orientation.Horizontal;
 				if (Widget.Loaded)
-					Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+					UpdatePosition();
 			}
 		}
 
@@ -380,7 +365,7 @@ namespace Eto.Mac.Forms.Controls
 				{
 					if (double.IsNaN(relative))
 						relative = RelativePosition;
-					Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+					UpdatePosition();
 				}
 				else if (WasLoaded)
 				{
@@ -461,7 +446,7 @@ namespace Eto.Mac.Forms.Controls
 			base.OnLoadComplete(e);
 			WasLoaded = false;
 			SetInitialSplitPosition();
-			Control.ResizeSubviewsWithOldSize(CGSize.Empty);
+			UpdatePosition();
 			initialPositionSet = true;
 		}
 
@@ -558,6 +543,11 @@ namespace Eto.Mac.Forms.Controls
 				size.Width = Math.Max(size1.Width, size2.Width);
 			}
 			return size;
+		}
+
+		void UpdatePosition()
+		{
+			Control.ResizeSubviewsWithOldSize(CGSize.Empty);
 		}
 	}
 }

Source/Eto.Mac/Forms/MacContainer.cs
@@ -128,7 +128,7 @@ namespace Eto.Mac.Forms
 			}
 		}
 
-		public void LayoutParent(bool updateSize = true)
+		public virtual void LayoutParent(bool updateSize = true)
 		{
 			if (NeedsQueue(() => LayoutParent(updateSize)))
 				return;