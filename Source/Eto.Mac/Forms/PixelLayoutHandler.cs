19d5955be7d7ca8ebd38c0ff3a5537c199d92aec
Curtis Wensley <curtis.wensley@gmail.com>
2016-02-06 12:05:01 -0800

    Mac: Optimize PixelLayout and when other controls have explicit sizes
    - Added Control.IsSuspended to determine if the control should suspend its layout.

--- Source/Eto.Mac/Forms/PixelLayoutHandler.cs
+++ Source/Eto.Mac/Forms/PixelLayoutHandler.cs
@@ -39,8 +39,6 @@ namespace Eto.Mac.Forms
 {
 	public class PixelLayoutHandler : MacContainer<NSView, PixelLayout, PixelLayout.ICallback>, PixelLayout.IHandler
 	{
-		readonly Dictionary<Control, PointF> points = new Dictionary<Control, PointF>();
-
 		public override NSView ContainerControl { get { return Control; } }
 
 		protected override NSView CreateControl()
@@ -48,64 +46,30 @@ namespace Eto.Mac.Forms
 			return new MacEventView();
 		}
 
-		public CGRect GetPosition(Control control)
-		{
-			PointF point;
-			if (points.TryGetValue(control, out point))
-			{
-				var frameSize = ((NSView)control.ControlObject).Frame.Size;
-				return new CGRect(point.ToNS(), frameSize);
-			}
-			return control.GetContainerView().Frame;
-		}
-
 		protected override SizeF GetNaturalSize(SizeF availableSize)
 		{
+			base.GetNaturalSize(availableSize);
 			SizeF size = SizeF.Empty;
-			foreach (var item in points.Where(r => r.Key.Visible))
+			foreach (var item in Widget.Controls)
 			{
-				var frameSize = item.Key.GetPreferredSize(availableSize);
-				size = SizeF.Max(size, frameSize + new SizeF(item.Value));
+				var frameSize = item.GetPreferredSize(availableSize);
+				size = SizeF.Max(size, frameSize + new SizeF(item.Location));
 			}
 			return size;
 		}
 
-		public override void OnLoadComplete(EventArgs e)
-		{
-			base.OnLoadComplete(e);
-			LayoutChildren();
-			Widget.SizeChanged += HandleSizeChanged;
-		}
-
-		public override void OnUnLoad(EventArgs e)
-		{
-			base.OnUnLoad(e);
-			Widget.SizeChanged -= HandleSizeChanged;
-		}
-
-		void HandleSizeChanged (object sender, EventArgs e)
-		{
-			LayoutChildren();
-		}
-
-		void SetPosition(Control control, PointF point, float frameHeight, bool flipped)
+		void SetPosition(Control control, PointF point)
 		{
 			var offset = ((IMacViewHandler)control.Handler).PositionOffset;
 			var childView = control.GetContainerView();
 			
 			var preferredSize = control.GetPreferredSize(Control.Frame.Size.ToEtoSize());
 
-			CGPoint origin;
-			if (flipped)
-				origin = new CGPoint(
-					point.X + offset.Width,
-					point.Y + offset.Height
-				);
-			else
-				origin = new CGPoint(
-					point.X + offset.Width,
-					frameHeight - (preferredSize.Height + point.Y + offset.Height)
-				);
+			var origin = new CGPoint(point.X + offset.Width, point.Y + offset.Height);
+			if (!Control.IsFlipped)
+			{
+				origin.Y = Control.Frame.Height - origin.Y - preferredSize.Height;
+			}
 
 			var frame = new CGRect(origin, preferredSize.ToNS());
 			if (frame != childView.Frame)
@@ -114,29 +78,12 @@ namespace Eto.Mac.Forms
 			}
 		}
 
-		public override void LayoutChildren()
-		{
-			if (NeedsQueue())
-				return;
-			var controlPoints = points.ToArray();
-			var frameHeight = Control.Frame.Height;
-			var flipped = Control.IsFlipped;
-			foreach (var item in controlPoints)
-			{
-				SetPosition(item.Key, item.Value, (float)frameHeight, flipped);
-			}
-		}
-
 		public void Add(Control child, int x, int y)
 		{
 			var location = new Point(x, y);
-			points[child] = location;
 			var childView = child.GetContainerView();
-			if (Widget.Loaded)
-			{
-				var frameHeight = Control.Frame.Height;
-				SetPosition(child, location, (float)frameHeight, Control.IsFlipped);
-			}
+			childView.AutoresizingMask = NSViewResizingMask.MinYMargin;
+			SetPosition(child, location);
 			Control.AddSubview(childView);
 			if (Widget.Loaded)
 				LayoutParent();
@@ -145,22 +92,14 @@ namespace Eto.Mac.Forms
 		public void Move(Control child, int x, int y)
 		{
 			var location = new Point(x, y);
-			if (points[child] != location)
-			{
-				points[child] = location;
-				if (Widget.Loaded)
-				{
-					var frameHeight = Control.Frame.Height;
-					SetPosition(child, location, (float)frameHeight, Control.IsFlipped);
-					LayoutParent();
-				}
-			}
+			SetPosition(child, location);
+			if (Widget.Loaded)
+				LayoutParent();
 		}
 
 		public void Remove(Control child)
 		{
 			var childView = child.GetContainerView();
-			points.Remove(child);
 			childView.RemoveFromSuperview();
 			if (Widget.Loaded)
 				LayoutParent();
