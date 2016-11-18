c0ee960099748156b41dc12da136ccec97229297
Curtis Wensley <curtis.wensley@gmail.com>
2016-11-18 11:55:12 -0800

    Gtk/Mac: Allow Slider.TickFrequency to be set to zero
    Fixes #563

---
+++ Source/Eto.Test/Eto.Test/UnitTests/Forms/Controls/SliderTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using Eto.Forms;
+
+namespace Eto.Test.UnitTests.Forms.Controls
+{
+	[TestFixture]
+	public class SliderTests : TestBase
+	{
+		[Test]
+		public void TickFrequencyShouldAllowZero()
+		{
+			Invoke(() =>
+			{
+				var slider = new Slider();
+				slider.TickFrequency = 0;
+				Assert.AreEqual(0, slider.TickFrequency);
+				slider.Value = 10;
+				slider.TickFrequency = 20;
+				Assert.AreEqual(20, slider.TickFrequency);
+				Assert.AreEqual(10, slider.Value);
+			});
+		}
+	}
+}
