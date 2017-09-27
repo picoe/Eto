d351b5fa22f9792fde975a4abdc56f4e97f32ce1
William Derby <derbyw@verizon.net>
2017-09-27 14:54:15 -0400

    add GTK3 specific version of GesturesEto

---
+++ Source/GesturesEto.Gtk3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Eto;
+using Eto.Forms;
+
+namespace GesturesEto.Gtk3
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main(string[] args)
+		{
+			new Application(Platforms.Gtk3).Run(new MainForm());
+		}
+	}
+}
