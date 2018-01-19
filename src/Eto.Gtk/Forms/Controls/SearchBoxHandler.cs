28fc08426f2304b42bc32987f0ea81327aa2e120
cra0zy <cra0zy@gmail.com>
2018-01-19 20:57:29 +0100

    [Eto.Gtk] Implement proper searchbox

--- src/Eto.Gtk/Forms/Controls/SearchBoxHandler.cs
+++ src/Eto.Gtk/Forms/Controls/SearchBoxHandler.cs
@@ -2,8 +2,19 @@ using Eto.Forms;
 
 namespace Eto.GtkSharp.Forms.Controls
 {
+#if GTKCORE
+	public class SearchBoxHandler : TextBoxHandler<Gtk.SearchEntry, SearchBox, SearchBox.ICallback>, SearchBox.IHandler
+	{
+		public SearchBoxHandler()
+		{
+			Control = new Gtk.SearchEntry();
+			Control.WidthRequest = 100;
+		}
+	}
+#else
 	public class SearchBoxHandler : TextBoxHandler, SearchBox.IHandler
 	{
 	}
+#endif
 }
 
