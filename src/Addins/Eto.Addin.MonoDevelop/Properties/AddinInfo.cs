0ad173fbce863220d8f70d4648cd3866f109dc62
Curtis Wensley <curtis.wensley@gmail.com>
2018-09-26 09:16:58 -0700

    Version updates

--- src/Addins/Eto.Addin.MonoDevelop/Properties/AddinInfo.cs
+++ src/Addins/Eto.Addin.MonoDevelop/Properties/AddinInfo.cs
@@ -5,7 +5,7 @@ using Mono.Addins.Description;
 [assembly:Addin(
 	"MonoDevelop", 
 	Namespace = "Eto.Addin",
-	Version = "2.4.0.0"
+	Version = "2.4.9999.0"
 )]
 
 [assembly:AddinName("Eto.Forms Support Addin")]
