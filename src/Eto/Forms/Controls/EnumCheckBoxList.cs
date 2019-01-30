13f797a42d58a2ce042da44f7644d63ff970a117
Curtis Wensley <curtis.wensley@gmail.com>
2019-01-30 14:25:55 -0800

    Add ItemToolTipBinding to CheckBoxList/RadioButtonList
    - Use binding for EnumCheckBoxList to show combined flags.
    - Fix EnumCheckBoxList.SelectedValues

--- src/Eto/Forms/Controls/EnumCheckBoxList.cs
+++ src/Eto/Forms/Controls/EnumCheckBoxList.cs
@@ -15,7 +15,11 @@ namespace Eto.Forms
 		{
 			public string Text { get; set; }
 
+			public string ToolTip { get; set; }
+
 			public string Key { get; set; }
+
+			public T Value { get; set; }
 		}
 
 		#region Events
@@ -37,13 +41,27 @@ namespace Eto.Forms
 		#endregion
 
 		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Eto.Forms.EnumCheckBoxList`1"/> class.
+		/// </summary>
+		public EnumCheckBoxList()
+		{
+			ItemToolTipBinding = Binding.Property((EnumValue m) => m.ToolTip);
+		}
+
+		/// <summary>
 		/// Gets or sets the currently selected enumeration value
 		/// </summary>
 		public new IEnumerable<T> SelectedValues
 		{
-			get { return base.SelectedValues.Cast<T>(); }
-			set { base.SelectedValues = value?.Cast<object>().ToList(); }
+			get { return base.SelectedValues.Cast<EnumValue>().Select(r => r.Value); }
+			set { base.SelectedValues = DataStore?.OfType<EnumValue>().Where(r => value.Contains(r.Value)) ?? Enumerable.Empty<object>(); }
 		}
+
+		/// <summary>
+		/// Gets or sets a value indicating whether to include a 'None' when the enumeration has the [Flags] attribute.
+		/// </summary>
+		/// <value><c>true</c> to include the none flag; otherwise, <c>false</c>.</value>
+		public bool IncludeNoneFlag { get; set; }
 
 		/// <summary>
 		/// Gets or sets a delegate used to get the text value for each item.
@@ -72,18 +90,33 @@ namespace Eto.Forms
 			var items = new ListItemCollection();
 			var values = Enum.GetValues(type);
 			var names = Enum.GetNames(type);
+			var isFlags = type.GetCustomAttribute<FlagsAttribute>(true) != null;
 			for (int i = 0; i < names.Length; i++)
 			{
-
-				var e = new AddValueEventArgs<T>((T)values.GetValue(i), true);
+				var objValue = values.GetValue(i);
+				if (isFlags && !IncludeNoneFlag && (int)objValue == 0)
+					continue;
+				var value = (T)objValue;
+				var e = new AddValueEventArgs<T>(value, true);
 				OnAddValue(e);
 				if (e.ShouldAdd)
 				{
-					items.Add(new EnumValue
+					var enumValue = new EnumValue
 					{
 						Text = GetText != null ? GetText(e.Value) : names[i],
-						Key = Convert.ToString(Convert.ToInt32(e.Value, CultureInfo.InvariantCulture), CultureInfo.InvariantCulture)
-					});
+						Key = Convert.ToString(Convert.ToInt32(e.Value, CultureInfo.InvariantCulture), CultureInfo.InvariantCulture),
+						Value = value
+					};
+					if (isFlags)
+					{
+						// show combined flags as tooltip
+						var includedFlags = values.OfType<object>().Where(r => !Equals(r, value) && (((int)objValue & (int)r) != 0)).ToList();
+						if (includedFlags.Count > 1)
+						{
+							enumValue.ToolTip = string.Join(" | ", includedFlags.Select(r => GetText != null ? GetText((T)r) : Enum.GetName(type, r)));
+						}
+					}
+					items.Add(enumValue);
 				}
 			}
 			if (SortAlphabetically)
