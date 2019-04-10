21a4b3177bd6a9cacdd6aa3c875b346a4b053229
Curtis Wensley <curtis.wensley@gmail.com>
2019-04-09 23:19:41 -0700

    Add other configurations for vscode
    - Can now run Wpf on windows and no longer prefers 32 bit so it can be debugged
    - Mac64 and Gtk2 on Mac or Linux both run using a modified vscode-mono-debug extension

---
+++ build/msbuild.cmd
@@ -0,0 +1,10 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set vswhere=%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe
+set version=15.0
+
+for /f "usebackq tokens=*" %%i in (`"%vswhere%" -version %version% -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+  "%%i" %*
+  exit /b !errorlevel!
+)
