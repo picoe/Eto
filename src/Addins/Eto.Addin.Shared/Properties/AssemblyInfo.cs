30c4b8b0df9103d6752c5d4b8aa201b84f5a53b6
Curtis Wensley <curtis.wensley@gmail.com>
2019-12-28 21:06:31 -0800

    Update VS addin for 2019

--- src/Addins/Eto.Addin.Shared/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("Eto.Addin.Shared")]
-[assembly: AssemblyDescription("")]
