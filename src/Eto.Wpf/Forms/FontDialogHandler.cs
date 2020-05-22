6dfc7416af3fdfcee87ec0eb8a6de64b282c7cd8
Curtis Wensley <curtis.wensley@gmail.com>
2020-05-22 15:10:56 -0700

    Wpf: Fire FontDialog.FontChanged event when changed while dialog is shown

--- src/Eto.Wpf/Forms/FontDialogHandler.cs
+++ src/Eto.Wpf/Forms/FontDialogHandler.cs
@@ -1,15 +1,26 @@
 using Eto.Drawing;
 using Eto.Forms;
+using Eto.Wpf.CustomControls.FontDialog;
 using Eto.Wpf.Drawing;
 using System;
 
 using sw = System.Windows;
 namespace Eto.Wpf.Forms
 {
-	public class FontDialogHandler : WidgetHandler<CustomControls.FontDialog.FontChooser, FontDialog, FontDialog.ICallback>, FontDialog.IHandler
+	public class FontDialogHandler : WidgetHandler<FontChooser, FontDialog, FontDialog.ICallback>, FontDialog.IHandler
 	{
+		Font _font;
+		static readonly object DisableFontChanged_Key = new object();
+
+		int DisableFontChanged
+		{
+			get => Widget.Properties.Get<int>(DisableFontChanged_Key);
+			set => Widget.Properties.Set(DisableFontChanged_Key, value);
+		}
+
 		public FontDialogHandler()
 		{
+			Control = new FontChooser();
 		}
 
 		public override void AttachEvent(string id)
@@ -17,7 +28,11 @@ namespace Eto.Wpf.Forms
 			switch (id)
 			{
 				case FontDialog.FontChangedEvent:
-					// handled during showdialog
+					Widget.Properties.Set(FontChooser.SelectedFontFamilyProperty, PropertyChangeNotifier.Register(FontChooser.SelectedFontFamilyProperty, FontChanged, Control));
+					Widget.Properties.Set(FontChooser.SelectedFontSizeProperty, PropertyChangeNotifier.Register(FontChooser.SelectedFontSizeProperty, FontChanged, Control));
+					Widget.Properties.Set(FontChooser.SelectedFontStretchProperty, PropertyChangeNotifier.Register(FontChooser.SelectedFontStretchProperty, FontChanged, Control));
+					Widget.Properties.Set(FontChooser.SelectedFontStyleProperty, PropertyChangeNotifier.Register(FontChooser.SelectedFontStyleProperty, FontChanged, Control));
+					Widget.Properties.Set(FontChooser.SelectedFontWeightProperty, PropertyChangeNotifier.Register(FontChooser.SelectedFontWeightProperty, FontChanged, Control));
 					break;
 				default:
 					base.AttachEvent(id);
@@ -25,42 +40,79 @@ namespace Eto.Wpf.Forms
 			}
 		}
 
+		private void FontChanged(object sender, EventArgs e)
+		{
+			if (DisableFontChanged > 0)
+				return;
+
+			if (IsChanged(_font))
+			{
+				_font = null;
+				Callback.OnFontChanged(Widget, EventArgs.Empty);
+			}
+		}
+
 		public Font Font
 		{
-			get;
-			set;
+			get => _font ?? (_font = GetFont());
+			set
+			{
+				if (!ReferenceEquals(_font, value))
+				{
+					_font = value;
+					if (_font?.Handler is FontHandler fontHandler && IsChanged(_font))
+					{
+						DisableFontChanged++;
+						Control.SelectedFontFamily = fontHandler.WpfFamily;
+						Control.SelectedFontPointSize = fontHandler.Size;
+						Control.SelectedFontStyle = fontHandler.WpfFontStyle;
+						Control.SelectedFontWeight = fontHandler.WpfFontWeight;
+						Control.SelectedFontStretch = fontHandler.WpfFontStretch;
+						DisableFontChanged--;
+						Callback.OnFontChanged(Widget, EventArgs.Empty);
+					}
+				}
+			}
+		}
+
+		private bool IsChanged(Font font)
+		{
+			if (font?.Handler is FontHandler fontHandler)
+			{
+				return Control.SelectedFontFamily?.Source != fontHandler.WpfFamily?.Source
+				|| Control.SelectedFontPointSize != fontHandler.Size
+				|| Control.SelectedFontStyle != fontHandler.WpfFontStyle
+				|| Control.SelectedFontWeight != fontHandler.WpfFontWeight
+				|| Control.SelectedFontStretch != fontHandler.WpfFontStretch;
+			}
+			return true;
+		}
+
+		Font GetFont()
+		{
+			var fontHandler = new FontHandler(Control.SelectedFontFamily, Control.SelectedFontPointSize, Control.SelectedFontStyle, Control.SelectedFontWeight, Control.SelectedFontStretch);
+			return new Font(fontHandler);
 		}
 
 		public DialogResult ShowDialog(Window parent)
 		{
-			Control = new CustomControls.FontDialog.FontChooser();
-			
 			if (parent != null)
 			{
 				var owner = parent.ControlObject as sw.Window;
 				Control.Owner = owner;
 				Control.WindowStartupLocation = sw.WindowStartupLocation.CenterOwner;
 			}
+			var lastFont = Font;
 
-			if (Font?.Handler is FontHandler fontHandler)
-			{
-				Control.SelectedFontFamily = fontHandler.WpfFamily;
-				Control.SelectedFontPointSize = fontHandler.Size;
-				Control.SelectedFontStyle = fontHandler.WpfFontStyle;
-				Control.SelectedFontWeight = fontHandler.WpfFontWeight;
-				Control.SelectedFontStretch = fontHandler.WpfFontStretch;
-			}
 			var result = Control.ShowDialog();
 
-			if (result == true)
+			if (result != true)
 			{
-				fontHandler = new FontHandler(Control.SelectedFontFamily, Control.SelectedFontPointSize, Control.SelectedFontStyle, Control.SelectedFontWeight, Control.SelectedFontStretch);
-				Font = new Font(fontHandler);
-				Callback.OnFontChanged(Widget, EventArgs.Empty);
-				return DialogResult.Ok;
+				// restore to original font when cancelled.
+				Font = lastFont;
 			}
 
-			return DialogResult.Cancel;
+			return result == true ? DialogResult.Ok : DialogResult.Cancel;
 		}
 	}
 }
