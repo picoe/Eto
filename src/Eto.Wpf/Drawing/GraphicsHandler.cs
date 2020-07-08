f0d7843a2c2f527ecf068928d9b807d2d31f4ed4
Curtis Wensley <curtis.wensley@gmail.com>
2020-07-08 15:13:14 -0700

    Misc fixes
    Mac: Fix possible NRE's for TextBox and mouse events
    Wpf: fix size of image after using Graphics on a non-alpha Bitmap

--- src/Eto.Wpf/Drawing/GraphicsHandler.cs
+++ src/Eto.Wpf/Drawing/GraphicsHandler.cs
@@ -363,7 +363,7 @@ namespace Eto.Wpf.Drawing
 				if (!bmp.Format.HasAlpha())
 				{
 					// convert to non-alpha, as RenderTargetBitmap does not support anything other than Pbgra32
-					var wb = new swmi.WriteableBitmap(bmp.PixelWidth, bmp.PixelWidth, bmp.DpiX, bmp.DpiY, swm.PixelFormats.Bgr32, null);
+					var wb = new swmi.WriteableBitmap(bmp.PixelWidth, bmp.PixelHeight, bmp.DpiX, bmp.DpiY, swm.PixelFormats.Bgr32, null);
 					var rect = new sw.Int32Rect(0, 0, bmp.PixelWidth, bmp.PixelHeight);
 					var pixels = new byte[bmp.PixelHeight * wb.BackBufferStride];
 					newbmp.CopyPixels(rect, pixels, wb.BackBufferStride, 0);
