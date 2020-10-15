a38a79f6dc6e62a8ccd6fddaa81471c1c44065f7
William M Derby Jr <derbyw@verizon.net>
2020-10-15 15:05:52 -0400

    fix slow leak of Pango context objects in drawable update

--- src/Eto.Gtk/Forms/Controls/DrawableHandler.cs
+++ src/Eto.Gtk/Forms/Controls/DrawableHandler.cs
@@ -91,7 +91,7 @@ namespace Eto.GtkSharp.Forms.Controls
 				if (!GraphicsHandler.GetClipRectangle(args.Cr, ref rect))
 					rect = new Gdk.Rectangle(Gdk.Point.Zero, allocation);
 
-				using (var graphics = new Graphics(new GraphicsHandler(args.Cr, h.Control.CreatePangoContext(), false)))
+				using (var graphics = new Graphics(new GraphicsHandler(args.Cr, h.Control.PangoContext, false)))
 				{
 					if (h.SelectedBackgroundColor != null)
 						graphics.Clear(h.SelectedBackgroundColor.Value);
