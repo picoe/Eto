d0450ef1b34bed3017559843a71e765f05a6014d
Curtis Wensley <curtis.wensley@gmail.com>
2021-03-31 10:50:02 -0700

    Improve performance of binding by not getting the existing value unless needed.

--- src/Eto/Forms/Binding/BindingChangedEventArgs.cs
+++ src/Eto/Forms/Binding/BindingChangedEventArgs.cs
@@ -12,15 +12,24 @@ namespace Eto.Forms
 		/// <summary>
 		/// Gets the value that was set to the binding
 		/// </summary>
-		public object Value { get; private set; }
-		
+		public object Value => InternalValue;
+
+		internal virtual object InternalValue { get; set; }
+
+		/// <summary>
+		/// Initializes a new instance of the BindingChangedEventArgs with the specified value
+		/// </summary>
+		/// <param name="value">value that the binding was set to</param>
+		public BindingChangedEventArgs(object value)
+		{
+			this.InternalValue = value;
+		}
+
 		/// <summary>
 		/// Initializes a new instance of the BindingChangedEventArgs
 		/// </summary>
-		/// <param name="value">value that the binding was set to</param>
-		public BindingChangedEventArgs (object value)
+		internal BindingChangedEventArgs()
 		{
-			this.Value = value;
 		}
 	}
 }
