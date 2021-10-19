983bf3ddef91cca8e3ec11b984fa193da95e5e11
Curtis Wensley <curtis.wensley@gmail.com>
2021-10-19 01:48:30 -0700

    Remove WinRT

--- src/Eto.WinRT/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Eto.Forms - Windows 8/Phone Platform")]
-[assembly: AssemblyDescription("Windows 8/Phone Platform for the Eto.Forms UI Framework")]
