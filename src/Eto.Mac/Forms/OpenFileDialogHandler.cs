35cf9140795a175759026a3bbf3465484d012c01
Curtis Wensley <curtis.wensley@gmail.com>
2025-04-22 11:16:22 -0700

    Mac: Fix some Open/SaveFileDialog issues
    - Show accessory view/file type filter for open file dialog by default
    - Fix crash after the delegate has been set but handler is GC'd
    - Clear out delegate after shown so it doesn't get used for other things

--- src/Eto.Mac/Forms/OpenFileDialogHandler.cs
+++ src/Eto.Mac/Forms/OpenFileDialogHandler.cs
@@ -3,20 +3,28 @@ namespace Eto.Mac.Forms
 	public class OpenFileDialogHandler : MacFileDialog<NSOpenPanel, OpenFileDialog>, OpenFileDialog.IHandler
 	{
 
-		protected override NSOpenPanel CreateControl()
-		{
-			return NSOpenPanel.OpenPanel;
-		}
+		protected override NSOpenPanel CreateControl() => NSOpenPanel.OpenPanel;
 
 		public bool MultiSelect
 		{
-			get { return Control.AllowsMultipleSelection; }
-			set { Control.AllowsMultipleSelection = value; }
+			get => Control.AllowsMultipleSelection;
+			set => Control.AllowsMultipleSelection = value;
 		}
 
-		public IEnumerable<string> Filenames
+		public IEnumerable<string> Filenames => Control.Urls.Select(a => a.Path);
+
+		static readonly Selector selSetAccessoryViewDisclosed = new Selector("setAccessoryViewDisclosed:");
+
+		internal override void Create()
 		{
-			get { return Control.Urls.Select(a => a.Path); }
+			base.Create();
+
+			if (Control.AccessoryView != null && Control.RespondsToSelector(selSetAccessoryViewDisclosed))
+			{
+				// ensure accessory view is always disclosed
+				// only available on NSOpenPanel.
+				Messaging.void_objc_msgSend_bool(Control.Handle, selSetAccessoryViewDisclosed.Handle, true);
+			}
 		}
 	}
 }
